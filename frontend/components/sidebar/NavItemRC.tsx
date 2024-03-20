94012d52ab8c490397250e53bcc25a8d8079c59a
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-20 20:33:37 +0600

    Refactor NavItemRC component styles

--- frontend/components/sidebar/NavItemRC.tsx
+++ frontend/components/sidebar/NavItemRC.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import RippleDivRC from "../effects/ripple/div/RippleDivRC";
-const navCss = `relative flex flex-row items-center border-transparent border-l-4
-hover:text-white focus:outline-none hover:bg-gray-500 hover:border-indigo-500 hover:text-indigo-500 hover:font-semibold hover:no-underline `
+const navCss = `relative flex flex-row items-center font-semibold 
+focus:outline-none hover:bg-indigo-50 hover:no-underline `
 
 type subItemType = { name: string, icon: any, route: string }
 
 const navLinkClasses = ({ isActive }: any) => {
-    return `${isActive ? 'bg-gray-500 border-l-green-600 font-semibold ' : ''}${navCss}`;
+    return `${isActive ? 'bg-indigo-100 text-indigo-700 rounded-md ' : ''}${navCss}`;
 };
 
 function NavLinkItem({ name, icon, route }: { name: string, icon: any, route: string, }) {
@@ -18,11 +18,11 @@ function NavLinkItem({ name, icon, route }: { name: string, icon: any, route: st
     return (
 
         <NavLink className={navLinkClasses} to={route} onClick={handleClick}>
-            <RippleDivRC className={'w-full h-11 flex items-center'}>
-                <span className="inline-flex justify-center text-white items-center ml-4">
+            <RippleDivRC className={'w-full h-10 flex rounded-md items-center'}>
+                <span className="inline-flex justify-center items-center ml-4">
                     {icon}
                 </span>
-                <span className="ml-2 text-sm text-white tracking-wide truncate">{name}</span>
+                <span className="ml-2 text-sm tracking-wide truncate">{name}</span>
             </RippleDivRC>
         </NavLink>
     );
@@ -36,12 +36,12 @@ function ChevronIcon({ isOpen }: { isOpen: boolean }) {
 // Dropdown button component
 function DropdownButton({ name, icon, isDropdownOpen, toggleDropdown }: { name: string, icon: any, isDropdownOpen: boolean, toggleDropdown: () => void }) {
     return (
-        <button type="button" className={`${navCss} w-full flex justify-between items-center text-white h-11 pr-6`} onClick={toggleDropdown}>
+        <button type="button" className={`${navCss} w-full flex justify-between items-center h-10 pr-6 ${isDropdownOpen ? 'shadow-[0_5px_5px_#dfe7ff]' : ''}`} onClick={toggleDropdown}>
             <div className="flex items-center">
-                <span className="inline-flex text-white justify-center items-center ml-4">
+                <span className="inline-flex justify-center items-center ml-4">
                     {icon}
                 </span>
-                <span className="ml-2 text-sm text-white tracking-wide truncate">{name}</span>
+                <span className="ml-2 text-sm tracking-wide truncate">{name}</span>
             </div>
             <ChevronIcon isOpen={isDropdownOpen} />
         </button>
@@ -51,7 +51,7 @@ function DropdownButton({ name, icon, isDropdownOpen, toggleDropdown }: { name: 
 // Component to display the list of sub items
 function SubItemList({ subItems }: { subItems: subItemType[] }) {
     return (
-        <div className="bg-gray-700 border-l-4 border-blue-500">
+        <div className="pl-4 border-b border-gray-300">
             <ul>
                 {subItems.map((item, index) => (
                     <li key={index}>
