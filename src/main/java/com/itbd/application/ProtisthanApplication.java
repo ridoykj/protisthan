64086bfcb90228c5f86d4e82c9dc6b46c83a5ccb
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-20 20:39:56 +0600

    Remove empty line in ProtisthanApplication.java

--- src/main/java/com/itbd/application/ProtisthanApplication.java
+++ src/main/java/com/itbd/application/ProtisthanApplication.java
@@ -20,5 +20,4 @@ public class ProtisthanApplication implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(ProtisthanApplication.class, args);
     }
-
 }
