7ee1b357ee30d57030072683a27f6a3ee942decf
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-10 23:39:03 +0600

    Add PersonalDataDeletionRequestDao, PersonalDataDeletionStepDao, and PersonalDataDownloadRequestDao classes

---
+++ src/main/java/com/itbd/application/db/dao/PersonalDataDeletionStepDao.java
@@ -0,0 +1,69 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_personal_data_deletion_step", indexes = {
+        @Index(name = "idx_parent", columnList = "tx_parent")
+})
+public class PersonalDataDeletionStepDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_document_type", length = 140)
+    private String documentType;
+
+    @Column(name = "tx_status", length = 140)
+    private String status;
+
+    @Column(name = "is_partial", nullable = false)
+    @ColumnDefault("0")
+    private Boolean partial;
+
+    @Column(name = "tx_fields", columnDefinition = "text")
+    private String fields;
+
+    @Column(name = "tx_filtered_by", length = 140)
+    private String filteredBy;
+
+    @Column(name = "tx_parent", length = 140)
+    private String parent;
+
+    @Column(name = "tx_parent_field", length = 140)
+    private String parentField;
+
+    @Column(name = "tx_parent_type", length = 140)
+    private String parentType;
+
+}
---
+++ src/main/java/com/itbd/application/db/dao/PersonalDataDownloadRequestDao.java
@@ -0,0 +1,65 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_personal_data_download_request", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class PersonalDataDownloadRequestDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_user", length = 140)
+    private String user;
+
+    @Column(name = "tx_user_name", length = 140)
+    private String userName;
+
+    @Column(name = "tx_amended_from", length = 140)
+    private String amendedFrom;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
