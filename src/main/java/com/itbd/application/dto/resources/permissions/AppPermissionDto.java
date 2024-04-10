ed7dc0d7ff7ea92e8a907505d1b0b0b9d620a690
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-10 23:45:50 +0600

    Refactor AppPermissionRepo and AppPermissionKey classes

--- src/main/java/com/itbd/application/dto/resources/permissions/AppPermissionDto.java
+++ src/main/java/com/itbd/application/dto/resources/permissions/AppPermissionDto.java
@@ -1,48 +1,48 @@
-package com.itbd.application.dto.resources.permissions;
-
-import com.itbd.application.dao.resources.AppResourceDao;
-import com.itbd.application.dao.resources.RolesDao;
-import com.itbd.application.dao.resources.permissions.AppPermissionDao;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
-
-import java.util.Set;
-
-public record AppPermissionDto(
-        @Id String resourceId,
-        @Id Long roleId,
-        @Version Long version,
-        Set<String> allowed,
-        RolesDao role,
-        AppResourceDao resource
-) {
-    public static AppPermissionDto fromEntity(AppPermissionDao appPermission) {
-//        RolesDao role = appPermission.getRole();
-//        role.setUsers(null);
-//        role.setPermissions(null);
-//        appPermission.setRole(role);
-
-//        AppResourceDao resource = appPermission.getResource();
-//        resource.setPermissions(null);
-//        appPermission.setResource(resource);
-
-        appPermission.setRole(null);
-        appPermission.setResource(null);
-        return new AppPermissionDto(
-                appPermission.getResourceId(),
-                appPermission.getRoleId(),
-                appPermission.getVersion(),
-                appPermission.getAllowed(),
-                appPermission.getRole(),
-                appPermission.getResource());
-    }
-
-    public static void fromDTO(AppPermissionDto appPermissionDTO, AppPermissionDao appPermissionDAO) {
-//        appPermissionDAO.setResourceId(appPermissionDTO.resourceId());
-//        appPermissionDAO.setRoleId(appPermissionDTO.roleId());
-        appPermissionDAO.setVersion(appPermissionDTO.version());
-        appPermissionDAO.setAllowed(appPermissionDTO.allowed());
-//        appPermissionDAO.setRole(appPermissionDTO.role());
-//        appPermissionDAO.setResource(appPermissionDTO.resource());
-    }
-}
+//package com.itbd.application.dto.resources.permissions;
+//
+//import com.itbd.application.dao.resources.AppResourceDao;
+//import com.itbd.application.dao.resources.RolesDao;
+//import com.itbd.application.dao.resources.permissions.AppPermissionDao;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Version;
+//
+//import java.util.Set;
+//
+//public record AppPermissionDto(
+//        @Id String resourceId,
+//        @Id Long roleId,
+//        @Version Long version,
+//        Set<String> allowed,
+//        RolesDao role,
+//        AppResourceDao resource
+//) {
+//    public static AppPermissionDto fromEntity(AppPermissionDao appPermission) {
+////        RolesDao role = appPermission.getRole();
+////        role.setUsers(null);
+////        role.setPermissions(null);
+////        appPermission.setRole(role);
+//
+////        AppResourceDao resource = appPermission.getResource();
+////        resource.setPermissions(null);
+////        appPermission.setResource(resource);
+//
+//        appPermission.setRole(null);
+//        appPermission.setResource(null);
+//        return new AppPermissionDto(
+//                appPermission.getResourceId(),
+//                appPermission.getRoleId(),
+//                appPermission.getVersion(),
+//                appPermission.getAllowed(),
+//                appPermission.getRole(),
+//                appPermission.getResource());
+//    }
+//
+//    public static void fromDTO(AppPermissionDto appPermissionDTO, AppPermissionDao appPermissionDAO) {
+////        appPermissionDAO.setResourceId(appPermissionDTO.resourceId());
+////        appPermissionDAO.setRoleId(appPermissionDTO.roleId());
+//        appPermissionDAO.setVersion(appPermissionDTO.version());
+//        appPermissionDAO.setAllowed(appPermissionDTO.allowed());
+////        appPermissionDAO.setRole(appPermissionDTO.role());
+////        appPermissionDAO.setResource(appPermissionDTO.resource());
+//    }
+//}
--- src/main/java/com/itbd/application/services/resources/permissions/AppPermissionDtoCrudService.java
+++ src/main/java/com/itbd/application/services/resources/permissions/AppPermissionDtoCrudService.java
@@ -1,63 +1,63 @@
-package com.itbd.application.services.resources.permissions;
-
-import com.itbd.application.dao.resources.permissions.AppPermissionDao;
-import com.itbd.application.dao.resources.permissions.AppPermissionKey;
-import com.itbd.application.dto.resources.permissions.AppPermissionDto;
-import com.itbd.application.repos.resources.permissions.AppPermissionRepo;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-import dev.hilla.BrowserCallable;
-import dev.hilla.Nonnull;
-import dev.hilla.Nullable;
-import dev.hilla.crud.CrudService;
-import dev.hilla.crud.JpaFilterConverter;
-import dev.hilla.crud.filter.Filter;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@BrowserCallable
-@AnonymousAllowed
-public class AppPermissionDtoCrudService implements CrudService<AppPermissionDto, AppPermissionKey> {
-
-    private final JpaFilterConverter jpaFilterConverter;
-
-    private final AppPermissionRepo permissionRepo;
-
-    public AppPermissionDtoCrudService(JpaFilterConverter jpaFilterConverter, AppPermissionRepo permissionRepo) {
-        this.jpaFilterConverter = jpaFilterConverter;
-        this.permissionRepo = permissionRepo;
-    }
-
-    @Override
-    @Nonnull
-    public List<@Nonnull AppPermissionDto> list(Pageable pageable, @Nullable Filter filter) {
-        // Basic list implementation that only covers pagination,
-        // but not sorting or filtering
-        Specification<AppPermissionDao> spec = filter != null
-                ? jpaFilterConverter.toSpec(filter, AppPermissionDao.class)
-                : Specification.anyOf();
-        Page<AppPermissionDao> persons = permissionRepo.findAll(spec, pageable);
-        return persons.stream().map(AppPermissionDto::fromEntity).toList();
-    }
-
-    @Override
-    @Transactional
-    public @Nullable AppPermissionDto save(AppPermissionDto value) {
-        boolean check = value.resourceId() != null && value.roleId() != null && !value.resourceId().isEmpty() && value.roleId() > 0;
-        AppPermissionKey key = new AppPermissionKey(value.resourceId(), value.roleId());
-//        AppPermissionDao person = permissionRepo.findById(key).orElseGet(AppPermissionDao::new);
-        AppPermissionDao person = check ? permissionRepo.getReferenceById(key) : new AppPermissionDao();
-
-        // person.setRecordComment(check ? "UPDATE" : "NEW");
-        AppPermissionDto.fromDTO(value, person);
-        return AppPermissionDto.fromEntity(permissionRepo.save(person));
-    }
-
-    @Override
-    public void delete(AppPermissionKey id) {
-        permissionRepo.deleteById(id);
-    }
-}
+//package com.itbd.application.services.resources.permissions;
+//
+//import com.itbd.application.dao.resources.permissions.AppPermissionDao;
+//import com.itbd.application.dao.resources.permissions.AppPermissionKey;
+//import com.itbd.application.dto.resources.permissions.AppPermissionDto;
+//import com.itbd.application.repos.resources.permissions.AppPermissionRepo;
+//import com.vaadin.flow.server.auth.AnonymousAllowed;
+//import dev.hilla.BrowserCallable;
+//import dev.hilla.Nonnull;
+//import dev.hilla.Nullable;
+//import dev.hilla.crud.CrudService;
+//import dev.hilla.crud.JpaFilterConverter;
+//import dev.hilla.crud.filter.Filter;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.domain.Specification;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@BrowserCallable
+//@AnonymousAllowed
+//public class AppPermissionDtoCrudService implements CrudService<AppPermissionDto, AppPermissionKey> {
+//
+//    private final JpaFilterConverter jpaFilterConverter;
+//
+//    private final AppPermissionRepo permissionRepo;
+//
+//    public AppPermissionDtoCrudService(JpaFilterConverter jpaFilterConverter, AppPermissionRepo permissionRepo) {
+//        this.jpaFilterConverter = jpaFilterConverter;
+//        this.permissionRepo = permissionRepo;
+//    }
+//
+//    @Override
+//    @Nonnull
+//    public List<@Nonnull AppPermissionDto> list(Pageable pageable, @Nullable Filter filter) {
+//        // Basic list implementation that only covers pagination,
+//        // but not sorting or filtering
+//        Specification<AppPermissionDao> spec = filter != null
+//                ? jpaFilterConverter.toSpec(filter, AppPermissionDao.class)
+//                : Specification.anyOf();
+//        Page<AppPermissionDao> persons = permissionRepo.findAll(spec, pageable);
+//        return persons.stream().map(AppPermissionDto::fromEntity).toList();
+//    }
+//
+//    @Override
+//    @Transactional
+//    public @Nullable AppPermissionDto save(AppPermissionDto value) {
+//        boolean check = value.resourceId() != null && value.roleId() != null && !value.resourceId().isEmpty() && value.roleId() > 0;
+//        AppPermissionKey key = new AppPermissionKey(value.resourceId(), value.roleId());
+////        AppPermissionDao person = permissionRepo.findById(key).orElseGet(AppPermissionDao::new);
+//        AppPermissionDao person = check ? permissionRepo.getReferenceById(key) : new AppPermissionDao();
+//
+//        // person.setRecordComment(check ? "UPDATE" : "NEW");
+//        AppPermissionDto.fromDTO(value, person);
+//        return AppPermissionDto.fromEntity(permissionRepo.save(person));
+//    }
+//
+//    @Override
+//    public void delete(AppPermissionKey id) {
+//        permissionRepo.deleteById(id);
+//    }
+//}
