c5f30a4f2b978105c9d0a81283ffce0d88c36b3b
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-10 23:48:29 +0600

    Add ActivityCostDao and ActivityCostDto classes

---
+++ src/main/java/com/itbd/application/db/dto/activitys/ActivityCostDto.java
@@ -0,0 +1,73 @@
+package com.itbd.application.db.dto.activitys;
+
+import com.itbd.application.db.dao.activitys.ActivityCostDao;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record ActivityCostDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String activityType,
+        String employee,
+        String employeeName,
+        String department,
+        BigDecimal billingRate,
+        BigDecimal costingRate,
+        String title,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static ActivityCostDto fromEntity(ActivityCostDao eActivityCostDao) {
+        return new ActivityCostDto(
+                eActivityCostDao.getName(),
+                eActivityCostDao.getCreation(),
+                eActivityCostDao.getModified(),
+                eActivityCostDao.getModifiedBy(),
+                eActivityCostDao.getOwner(),
+                eActivityCostDao.getIsDocStatus(),
+                eActivityCostDao.getIdx(),
+                eActivityCostDao.getActivityType(),
+                eActivityCostDao.getEmployee(),
+                eActivityCostDao.getEmployeeName(),
+                eActivityCostDao.getDepartment(),
+                eActivityCostDao.getBillingRate(),
+                eActivityCostDao.getCostingRate(),
+                eActivityCostDao.getTitle(),
+                eActivityCostDao.getUserTags(),
+                eActivityCostDao.getComments(),
+                eActivityCostDao.getAssign(),
+                eActivityCostDao.getLikedBy());
+    }
+
+    public static void fromDTO(ActivityCostDto vActivityCostDto, ActivityCostDao eActivityCostDao) {
+        eActivityCostDao.setName(vActivityCostDto.name());
+        eActivityCostDao.setCreation(vActivityCostDto.creation());
+        eActivityCostDao.setModified(vActivityCostDto.modified());
+        eActivityCostDao.setModifiedBy(vActivityCostDto.modifiedBy());
+        eActivityCostDao.setOwner(vActivityCostDto.owner());
+        eActivityCostDao.setIsDocStatus(vActivityCostDto.isDocStatus());
+        eActivityCostDao.setIdx(vActivityCostDto.idx());
+        eActivityCostDao.setActivityType(vActivityCostDto.activityType());
+        eActivityCostDao.setEmployee(vActivityCostDto.employee());
+        eActivityCostDao.setEmployeeName(vActivityCostDto.employeeName());
+        eActivityCostDao.setDepartment(vActivityCostDto.department());
+        eActivityCostDao.setBillingRate(vActivityCostDto.billingRate());
+        eActivityCostDao.setCostingRate(vActivityCostDto.costingRate());
+        eActivityCostDao.setTitle(vActivityCostDto.title());
+        eActivityCostDao.setUserTags(vActivityCostDto.userTags());
+        eActivityCostDao.setComments(vActivityCostDto.comments());
+        eActivityCostDao.setAssign(vActivityCostDto.assign());
+        eActivityCostDao.setLikedBy(vActivityCostDto.likedBy());
+    }
+}
