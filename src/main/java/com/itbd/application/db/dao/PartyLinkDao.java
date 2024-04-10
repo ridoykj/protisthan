81d994dfa24df2cab823a740f6de271918dbb3ba
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-10 23:36:44 +0600

    Add PartyAccountDao, PartyTypeDao, and PartyLinkDao classes

---
+++ src/main/java/com/itbd/application/db/dao/PartyLinkDao.java
@@ -0,0 +1,68 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_party_link", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class PartyLinkDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_primary_role", length = 140)
+    private String primaryRole;
+
+    @Column(name = "tx_secondary_role", length = 140)
+    private String secondaryRole;
+
+    @Column(name = "tx_primary_party", length = 140)
+    private String primaryParty;
+
+    @Column(name = "tx_secondary_party", length = 140)
+    private String secondaryParty;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
