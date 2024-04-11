4c15840e8f021441d0cdbe376480f481625dac5b
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:26:20 +0600

    Add CallLogDao and CallLogDto classes for mapping CallLog entity

---
+++ src/main/java/com/itbd/application/db/dao/CallLogDao.java
@@ -0,0 +1,105 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_call_log", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+}, uniqueConstraints = {
+        @UniqueConstraint(name = "uk_id", columnNames = {"tx_id"})
+})
+public class CallLogDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_id", unique = true, length = 140)
+    private String id;
+
+    @Column(name = "tx_fromm", length = 140)
+    private String fromm;
+
+    @Column(name = "tx_to", length = 140)
+    private String to;
+
+    @Column(name = "tx_call_received_by", length = 140)
+    private String callReceivedBy;
+
+    @Column(name = "tx_employee_user_id", length = 140)
+    private String employeeUserId;
+
+    @Column(name = "tx_medium", length = 140)
+    private String medium;
+
+    @Column(name = "dtt_start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "dtt_end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "tx_type", length = 140)
+    private String type;
+
+    @Column(name = "tx_customer", length = 140)
+    private String customer;
+
+    @Column(name = "tx_status", length = 140)
+    private String status;
+
+    @Column(name = "flt_duration", precision = 21, scale = 9)
+    @ColumnDefault("0.0")
+    private BigDecimal duration;
+
+    @Column(name = "tx_recording_url", length = 140)
+    private String recordingUrl;
+
+    @Column(name = "tx_type_of_call", length = 140)
+    private String typeOfCall;
+
+    @Column(name = "tx_summary", columnDefinition = "text")
+    private String summary;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/CallLogDto.java
@@ -0,0 +1,97 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.CallLogDao;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record CallLogDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String id,
+        String fromm,
+        String to,
+        String callReceivedBy,
+        String employeeUserId,
+        String medium,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        String type,
+        String customer,
+        String status,
+        BigDecimal duration,
+        String recordingUrl,
+        String typeOfCall,
+        String summary,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static CallLogDto fromEntity(CallLogDao eCallLogDao) {
+        return new CallLogDto(
+                eCallLogDao.getName(),
+                eCallLogDao.getCreation(),
+                eCallLogDao.getModified(),
+                eCallLogDao.getModifiedBy(),
+                eCallLogDao.getOwner(),
+                eCallLogDao.getIsDocStatus(),
+                eCallLogDao.getIdx(),
+                eCallLogDao.getId(),
+                eCallLogDao.getFromm(),
+                eCallLogDao.getTo(),
+                eCallLogDao.getCallReceivedBy(),
+                eCallLogDao.getEmployeeUserId(),
+                eCallLogDao.getMedium(),
+                eCallLogDao.getStartTime(),
+                eCallLogDao.getEndTime(),
+                eCallLogDao.getType(),
+                eCallLogDao.getCustomer(),
+                eCallLogDao.getStatus(),
+                eCallLogDao.getDuration(),
+                eCallLogDao.getRecordingUrl(),
+                eCallLogDao.getTypeOfCall(),
+                eCallLogDao.getSummary(),
+                eCallLogDao.getUserTags(),
+                eCallLogDao.getComments(),
+                eCallLogDao.getAssign(),
+                eCallLogDao.getLikedBy());
+    }
+
+    public static void fromDTO(CallLogDto vCallLogDto, CallLogDao eCallLogDao) {
+        eCallLogDao.setName(vCallLogDto.name());
+        eCallLogDao.setCreation(vCallLogDto.creation());
+        eCallLogDao.setModified(vCallLogDto.modified());
+        eCallLogDao.setModifiedBy(vCallLogDto.modifiedBy());
+        eCallLogDao.setOwner(vCallLogDto.owner());
+        eCallLogDao.setIsDocStatus(vCallLogDto.isDocStatus());
+        eCallLogDao.setIdx(vCallLogDto.idx());
+        eCallLogDao.setId(vCallLogDto.id());
+        eCallLogDao.setFromm(vCallLogDto.fromm());
+        eCallLogDao.setTo(vCallLogDto.to());
+        eCallLogDao.setCallReceivedBy(vCallLogDto.callReceivedBy());
+        eCallLogDao.setEmployeeUserId(vCallLogDto.employeeUserId());
+        eCallLogDao.setMedium(vCallLogDto.medium());
+        eCallLogDao.setStartTime(vCallLogDto.startTime());
+        eCallLogDao.setEndTime(vCallLogDto.endTime());
+        eCallLogDao.setType(vCallLogDto.type());
+        eCallLogDao.setCustomer(vCallLogDto.customer());
+        eCallLogDao.setStatus(vCallLogDto.status());
+        eCallLogDao.setDuration(vCallLogDto.duration());
+        eCallLogDao.setRecordingUrl(vCallLogDto.recordingUrl());
+        eCallLogDao.setTypeOfCall(vCallLogDto.typeOfCall());
+        eCallLogDao.setSummary(vCallLogDto.summary());
+        eCallLogDao.setUserTags(vCallLogDto.userTags());
+        eCallLogDao.setComments(vCallLogDto.comments());
+        eCallLogDao.setAssign(vCallLogDto.assign());
+        eCallLogDao.setLikedBy(vCallLogDto.likedBy());
+    }
+}
