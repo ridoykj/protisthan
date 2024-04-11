5567125ecd92f80bc7b0ff932e579f8bece578f7
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:33:47 +0600

    Add VehicleRepository and VehicleDao classes

---
+++ src/main/java/com/itbd/application/db/dao/VehicleDao.java
@@ -0,0 +1,121 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_vehicle", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+}, uniqueConstraints = {
+        @UniqueConstraint(name = "uk_license_plate", columnNames = "tx_license_plate")
+})
+public class VehicleDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_license_plate", unique = true, length = 140)
+    private String licensePlate;
+
+    @Column(name = "tx_make", length = 140)
+    private String make;
+
+    @Column(name = "tx_model", length = 140)
+    private String model;
+
+    @Column(name = "ct_last_odometer", nullable = false)
+    private Integer lastOdometer;
+
+    @Column(name = "dt_acquisition_date")
+    private LocalDate acquisitionDate;
+
+    @Column(name = "tx_location", length = 140)
+    private String location;
+
+    @Column(name = "tx_chassis_no", length = 140)
+    private String chassisNo;
+
+    @Column(name = "flt_vehicle_value", nullable = false, precision = 21, scale = 9)
+    @ColumnDefault("0.0")
+    private BigDecimal vehicleValue;
+
+    @Column(name = "tx_employee", length = 140)
+    private String employee;
+
+    @Column(name = "tx_insurance_company", length = 140)
+    private String insuranceCompany;
+
+    @Column(name = "tx_policy_no", length = 140)
+    private String policyNo;
+
+    @Column(name = "dt_start_date")
+    private LocalDate startDate;
+
+    @Column(name = "dt_end_date")
+    private LocalDate endDate;
+
+    @Column(name = "tx_fuel_type", length = 140)
+    private String fuelType;
+
+    @Column(name = "tx_uom", length = 140)
+    private String uom;
+
+    @Column(name = "dt_carbon_check_date")
+    private LocalDate carbonCheckDate;
+
+    @Column(name = "tx_color", length = 140)
+    private String color;
+
+    @Column(name = "ct_wheels", nullable = false)
+    private Integer wheels;
+
+    @Column(name = "ct_doors", nullable = false)
+    private Integer doors;
+
+    @Column(name = "tx_amended_from", length = 140)
+    private String amendedFrom;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/VehicleDto.java
@@ -0,0 +1,113 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.VehicleDao;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record VehicleDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String licensePlate,
+        String make,
+        String model,
+        Integer lastOdometer,
+        LocalDate acquisitionDate,
+        String location,
+        String chassisNo,
+        BigDecimal vehicleValue,
+        String employee,
+        String insuranceCompany,
+        String policyNo,
+        LocalDate startDate,
+        LocalDate endDate,
+        String fuelType,
+        String uom,
+        LocalDate carbonCheckDate,
+        String color,
+        Integer wheels,
+        Integer doors,
+        String amendedFrom,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static VehicleDto fromEntity(VehicleDao eVehicleDao) {
+        return new VehicleDto(
+                eVehicleDao.getName(),
+                eVehicleDao.getCreation(),
+                eVehicleDao.getModified(),
+                eVehicleDao.getModifiedBy(),
+                eVehicleDao.getOwner(),
+                eVehicleDao.getIsDocStatus(),
+                eVehicleDao.getIdx(),
+                eVehicleDao.getLicensePlate(),
+                eVehicleDao.getMake(),
+                eVehicleDao.getModel(),
+                eVehicleDao.getLastOdometer(),
+                eVehicleDao.getAcquisitionDate(),
+                eVehicleDao.getLocation(),
+                eVehicleDao.getChassisNo(),
+                eVehicleDao.getVehicleValue(),
+                eVehicleDao.getEmployee(),
+                eVehicleDao.getInsuranceCompany(),
+                eVehicleDao.getPolicyNo(),
+                eVehicleDao.getStartDate(),
+                eVehicleDao.getEndDate(),
+                eVehicleDao.getFuelType(),
+                eVehicleDao.getUom(),
+                eVehicleDao.getCarbonCheckDate(),
+                eVehicleDao.getColor(),
+                eVehicleDao.getWheels(),
+                eVehicleDao.getDoors(),
+                eVehicleDao.getAmendedFrom(),
+                eVehicleDao.getUserTags(),
+                eVehicleDao.getComments(),
+                eVehicleDao.getAssign(),
+                eVehicleDao.getLikedBy());
+    }
+
+    public static void fromDTO(VehicleDto vVehicleDto, VehicleDao eVehicleDao) {
+        eVehicleDao.setName(vVehicleDto.name());
+        eVehicleDao.setCreation(vVehicleDto.creation());
+        eVehicleDao.setModified(vVehicleDto.modified());
+        eVehicleDao.setModifiedBy(vVehicleDto.modifiedBy());
+        eVehicleDao.setOwner(vVehicleDto.owner());
+        eVehicleDao.setIsDocStatus(vVehicleDto.isDocStatus());
+        eVehicleDao.setIdx(vVehicleDto.idx());
+        eVehicleDao.setLicensePlate(vVehicleDto.licensePlate());
+        eVehicleDao.setMake(vVehicleDto.make());
+        eVehicleDao.setModel(vVehicleDto.model());
+        eVehicleDao.setLastOdometer(vVehicleDto.lastOdometer());
+        eVehicleDao.setAcquisitionDate(vVehicleDto.acquisitionDate());
+        eVehicleDao.setLocation(vVehicleDto.location());
+        eVehicleDao.setChassisNo(vVehicleDto.chassisNo());
+        eVehicleDao.setVehicleValue(vVehicleDto.vehicleValue());
+        eVehicleDao.setEmployee(vVehicleDto.employee());
+        eVehicleDao.setInsuranceCompany(vVehicleDto.insuranceCompany());
+        eVehicleDao.setPolicyNo(vVehicleDto.policyNo());
+        eVehicleDao.setStartDate(vVehicleDto.startDate());
+        eVehicleDao.setEndDate(vVehicleDto.endDate());
+        eVehicleDao.setFuelType(vVehicleDto.fuelType());
+        eVehicleDao.setUom(vVehicleDto.uom());
+        eVehicleDao.setCarbonCheckDate(vVehicleDto.carbonCheckDate());
+        eVehicleDao.setColor(vVehicleDto.color());
+        eVehicleDao.setWheels(vVehicleDto.wheels());
+        eVehicleDao.setDoors(vVehicleDto.doors());
+        eVehicleDao.setAmendedFrom(vVehicleDto.amendedFrom());
+        eVehicleDao.setUserTags(vVehicleDto.userTags());
+        eVehicleDao.setComments(vVehicleDto.comments());
+        eVehicleDao.setAssign(vVehicleDto.assign());
+        eVehicleDao.setLikedBy(vVehicleDto.likedBy());
+    }
+}
