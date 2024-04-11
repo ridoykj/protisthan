bed701612c135222ef32c3ffe9f019c9ca76aa3c
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:24:40 +0600

    Add PreparedReportDao, PreparedReportDto, and PreparedReportRepository classes

---
+++ src/main/java/com/itbd/application/db/dao/PreparedReportDao.java
@@ -0,0 +1,77 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_prepared_report", indexes = {
+        @Index(name = "idx_status", columnList = "tx_status"),
+        @Index(name = "idx_report_name", columnList = "tx_report_name"),
+        @Index(name = "idx_modified", columnList = "dtt_modified"),
+})
+public class PreparedReportDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_status", length = 140)
+    @ColumnDefault("'Queued'")
+    private String status;
+
+    @Column(name = "tx_report_name", length = 140)
+    private String reportName;
+
+    @Column(name = "tx_job_id", length = 140)
+    private String jobId;
+
+    @Column(name = "dtt_report_end_time")
+    private LocalDateTime reportEndTime;
+
+    @Column(name = "tx_error_message", columnDefinition = "text")
+    private String errorMessage;
+
+    @Column(name = "tx_filters", columnDefinition = "text")
+    private String filters;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/PreparedReportDto.java
@@ -0,0 +1,69 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.PreparedReportDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record PreparedReportDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String status,
+        String reportName,
+        String jobId,
+        LocalDateTime reportEndTime,
+        String errorMessage,
+        String filters,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static PreparedReportDto fromEntity(PreparedReportDao ePreparedReportDao) {
+        return new PreparedReportDto(
+                ePreparedReportDao.getName(),
+                ePreparedReportDao.getCreation(),
+                ePreparedReportDao.getModified(),
+                ePreparedReportDao.getModifiedBy(),
+                ePreparedReportDao.getOwner(),
+                ePreparedReportDao.getIsDocStatus(),
+                ePreparedReportDao.getIdx(),
+                ePreparedReportDao.getStatus(),
+                ePreparedReportDao.getReportName(),
+                ePreparedReportDao.getJobId(),
+                ePreparedReportDao.getReportEndTime(),
+                ePreparedReportDao.getErrorMessage(),
+                ePreparedReportDao.getFilters(),
+                ePreparedReportDao.getUserTags(),
+                ePreparedReportDao.getComments(),
+                ePreparedReportDao.getAssign(),
+                ePreparedReportDao.getLikedBy());
+    }
+
+    public static void fromDTO(PreparedReportDto vPreparedReportDto, PreparedReportDao ePreparedReportDao) {
+        ePreparedReportDao.setName(vPreparedReportDto.name());
+        ePreparedReportDao.setCreation(vPreparedReportDto.creation());
+        ePreparedReportDao.setModified(vPreparedReportDto.modified());
+        ePreparedReportDao.setModifiedBy(vPreparedReportDto.modifiedBy());
+        ePreparedReportDao.setOwner(vPreparedReportDto.owner());
+        ePreparedReportDao.setIsDocStatus(vPreparedReportDto.isDocStatus());
+        ePreparedReportDao.setIdx(vPreparedReportDto.idx());
+        ePreparedReportDao.setStatus(vPreparedReportDto.status());
+        ePreparedReportDao.setReportName(vPreparedReportDto.reportName());
+        ePreparedReportDao.setJobId(vPreparedReportDto.jobId());
+        ePreparedReportDao.setReportEndTime(vPreparedReportDto.reportEndTime());
+        ePreparedReportDao.setErrorMessage(vPreparedReportDto.errorMessage());
+        ePreparedReportDao.setFilters(vPreparedReportDto.filters());
+        ePreparedReportDao.setUserTags(vPreparedReportDto.userTags());
+        ePreparedReportDao.setComments(vPreparedReportDto.comments());
+        ePreparedReportDao.setAssign(vPreparedReportDto.assign());
+        ePreparedReportDao.setLikedBy(vPreparedReportDto.likedBy());
+    }
+}
