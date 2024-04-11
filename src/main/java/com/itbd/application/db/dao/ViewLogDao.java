036348b1290d21b5cbdb5a69aa93299d28455879
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:34:50 +0600

    Add ViewLogDao, ViewLogDto, and ViewLogRepository classes

---
+++ src/main/java/com/itbd/application/db/dao/ViewLogDao.java
@@ -0,0 +1,66 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_view_log", indexes = {
+        @Index(name = "idx_reference_name", columnList = "tx_reference_name"),
+        @Index(name = "idx_modified", columnList = "dtt_modified"),
+})
+public class ViewLogDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_viewed_by", length = 140)
+    private String viewedBy;
+
+    @Column(name = "tx_reference_doc_type", length = 140)
+    private String referenceDocType;
+
+    @Column(name = "tx_reference_name", length = 140)
+    private String referenceName;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/ViewLogDto.java
@@ -0,0 +1,60 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.ViewLogDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record ViewLogDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String viewedBy,
+        String referenceDocType,
+        String referenceName,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static ViewLogDto fromEntity(ViewLogDao eViewLogDao) {
+        return new ViewLogDto(
+                eViewLogDao.getName(),
+                eViewLogDao.getCreation(),
+                eViewLogDao.getModified(),
+                eViewLogDao.getModifiedBy(),
+                eViewLogDao.getOwner(),
+                eViewLogDao.getIsDocStatus(),
+                eViewLogDao.getIdx(),
+                eViewLogDao.getViewedBy(),
+                eViewLogDao.getReferenceDocType(),
+                eViewLogDao.getReferenceName(),
+                eViewLogDao.getUserTags(),
+                eViewLogDao.getComments(),
+                eViewLogDao.getAssign(),
+                eViewLogDao.getLikedBy());
+    }
+
+    public static void fromDTO(ViewLogDto vViewLogDto, ViewLogDao eViewLogDao) {
+        eViewLogDao.setName(vViewLogDto.name());
+        eViewLogDao.setCreation(vViewLogDto.creation());
+        eViewLogDao.setModified(vViewLogDto.modified());
+        eViewLogDao.setModifiedBy(vViewLogDto.modifiedBy());
+        eViewLogDao.setOwner(vViewLogDto.owner());
+        eViewLogDao.setIsDocStatus(vViewLogDto.isDocStatus());
+        eViewLogDao.setIdx(vViewLogDto.idx());
+        eViewLogDao.setViewedBy(vViewLogDto.viewedBy());
+        eViewLogDao.setReferenceDocType(vViewLogDto.referenceDocType());
+        eViewLogDao.setReferenceName(vViewLogDto.referenceName());
+        eViewLogDao.setUserTags(vViewLogDto.userTags());
+        eViewLogDao.setComments(vViewLogDto.comments());
+        eViewLogDao.setAssign(vViewLogDto.assign());
+        eViewLogDao.setLikedBy(vViewLogDto.likedBy());
+    }
+}
