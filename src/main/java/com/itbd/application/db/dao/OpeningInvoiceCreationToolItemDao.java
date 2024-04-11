7ce09a5b3ce9cdcfacdd05c9e68fb65cd9b8543f
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 17:50:52 +0600

    Add OpeningInvoiceCreationToolItemRepository and OpeningInvoiceCreationToolItemDao classes for mapping Opening Invoice Creation Tool Item-related entities

---
+++ src/main/java/com/itbd/application/db/dao/OpeningInvoiceCreationToolItemDao.java
@@ -0,0 +1,88 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_opening_invoice_creation_tool_item", indexes = {
+        @Index(name = "idx_parent", columnList = "tx_parent")
+})
+public class OpeningInvoiceCreationToolItemDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_invoice_number", length = 140)
+    private String invoiceNumber;
+
+    @Column(name = "tx_party_type", length = 140)
+    private String partyType;
+
+    @Column(name = "tx_party", length = 140)
+    private String party;
+
+    @Column(name = "tx_temporary_opening_account", length = 140)
+    private String temporaryOpeningAccount;
+
+    @Column(name = "dt_posting_date")
+    private LocalDate postingDate;
+
+    @Column(name = "dt_dueDate")
+    private LocalDate dueDate;
+
+    @Column(name = "tx_item_name", length = 140)
+    @ColumnDefault("'Opening_Invoice_Item'")
+    private String itemName;
+
+    @Column(name = "flt_outstanding_amount", nullable = false, precision = 21, scale = 9)
+    @ColumnDefault("0.0")
+    private BigDecimal outstandingAmount;
+
+    @Column(name = "tx_qty", length = 140)
+    @ColumnDefault("'1'")
+    private String qty;
+
+    @Column(name = "tx_cost_center", length = 140)
+    private String costCenter;
+
+    @Column(name = "tx_parent", length = 140)
+    private String parent;
+
+    @Column(name = "tx_parent_field", length = 140)
+    private String parentField;
+
+    @Column(name = "tx_parent_type", length = 140)
+    private String parentType;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/OpeningInvoiceCreationToolItemDto.java
@@ -0,0 +1,80 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.OpeningInvoiceCreationToolItemDao;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record OpeningInvoiceCreationToolItemDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String invoiceNumber,
+        String partyType,
+        String party,
+        String temporaryOpeningAccount,
+        LocalDate postingDate,
+        LocalDate dueDate,
+        String itemName,
+        BigDecimal outstandingAmount,
+        String qty,
+        String costCenter,
+        String parent,
+        String parentField,
+        String parentType
+) {
+    public static OpeningInvoiceCreationToolItemDto fromEntity(OpeningInvoiceCreationToolItemDao eOpeningInvoiceCreationToolItemDao) {
+        return new OpeningInvoiceCreationToolItemDto(
+                eOpeningInvoiceCreationToolItemDao.getName(),
+                eOpeningInvoiceCreationToolItemDao.getCreation(),
+                eOpeningInvoiceCreationToolItemDao.getModified(),
+                eOpeningInvoiceCreationToolItemDao.getModifiedBy(),
+                eOpeningInvoiceCreationToolItemDao.getOwner(),
+                eOpeningInvoiceCreationToolItemDao.getIsDocStatus(),
+                eOpeningInvoiceCreationToolItemDao.getIdx(),
+                eOpeningInvoiceCreationToolItemDao.getInvoiceNumber(),
+                eOpeningInvoiceCreationToolItemDao.getPartyType(),
+                eOpeningInvoiceCreationToolItemDao.getParty(),
+                eOpeningInvoiceCreationToolItemDao.getTemporaryOpeningAccount(),
+                eOpeningInvoiceCreationToolItemDao.getPostingDate(),
+                eOpeningInvoiceCreationToolItemDao.getDueDate(),
+                eOpeningInvoiceCreationToolItemDao.getItemName(),
+                eOpeningInvoiceCreationToolItemDao.getOutstandingAmount(),
+                eOpeningInvoiceCreationToolItemDao.getQty(),
+                eOpeningInvoiceCreationToolItemDao.getCostCenter(),
+                eOpeningInvoiceCreationToolItemDao.getParent(),
+                eOpeningInvoiceCreationToolItemDao.getParentField(),
+                eOpeningInvoiceCreationToolItemDao.getParentType());
+    }
+
+    public static void fromDTO(OpeningInvoiceCreationToolItemDto vOpeningInvoiceCreationToolItemDto, OpeningInvoiceCreationToolItemDao eOpeningInvoiceCreationToolItemDao) {
+        eOpeningInvoiceCreationToolItemDao.setName(vOpeningInvoiceCreationToolItemDto.name());
+        eOpeningInvoiceCreationToolItemDao.setCreation(vOpeningInvoiceCreationToolItemDto.creation());
+        eOpeningInvoiceCreationToolItemDao.setModified(vOpeningInvoiceCreationToolItemDto.modified());
+        eOpeningInvoiceCreationToolItemDao.setModifiedBy(vOpeningInvoiceCreationToolItemDto.modifiedBy());
+        eOpeningInvoiceCreationToolItemDao.setOwner(vOpeningInvoiceCreationToolItemDto.owner());
+        eOpeningInvoiceCreationToolItemDao.setIsDocStatus(vOpeningInvoiceCreationToolItemDto.isDocStatus());
+        eOpeningInvoiceCreationToolItemDao.setIdx(vOpeningInvoiceCreationToolItemDto.idx());
+        eOpeningInvoiceCreationToolItemDao.setInvoiceNumber(vOpeningInvoiceCreationToolItemDto.invoiceNumber());
+        eOpeningInvoiceCreationToolItemDao.setPartyType(vOpeningInvoiceCreationToolItemDto.partyType());
+        eOpeningInvoiceCreationToolItemDao.setParty(vOpeningInvoiceCreationToolItemDto.party());
+        eOpeningInvoiceCreationToolItemDao.setTemporaryOpeningAccount(vOpeningInvoiceCreationToolItemDto.temporaryOpeningAccount());
+        eOpeningInvoiceCreationToolItemDao.setPostingDate(vOpeningInvoiceCreationToolItemDto.postingDate());
+        eOpeningInvoiceCreationToolItemDao.setDueDate(vOpeningInvoiceCreationToolItemDto.dueDate());
+        eOpeningInvoiceCreationToolItemDao.setItemName(vOpeningInvoiceCreationToolItemDto.itemName());
+        eOpeningInvoiceCreationToolItemDao.setOutstandingAmount(vOpeningInvoiceCreationToolItemDto.outstandingAmount());
+        eOpeningInvoiceCreationToolItemDao.setQty(vOpeningInvoiceCreationToolItemDto.qty());
+        eOpeningInvoiceCreationToolItemDao.setCostCenter(vOpeningInvoiceCreationToolItemDto.costCenter());
+        eOpeningInvoiceCreationToolItemDao.setParent(vOpeningInvoiceCreationToolItemDto.parent());
+        eOpeningInvoiceCreationToolItemDao.setParentField(vOpeningInvoiceCreationToolItemDto.parentField());
+        eOpeningInvoiceCreationToolItemDao.setParentType(vOpeningInvoiceCreationToolItemDto.parentType());
+    }
+}
