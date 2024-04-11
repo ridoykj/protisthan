8fa8012006ed57c12d09062a8d12904140d86f50
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:09:34 +0600

    Refactor RoomRepo and RoomDao classes

--- src/main/java/com/itbd/application/dao/org/place/RoomDao.java
+++ src/main/java/com/itbd/application/dao/org/place/RoomDao.java
@@ -1,76 +1,77 @@
-package com.itbd.application.dao.org.place;
+//package com.itbd.application.dao.org.place;
+//
+//import com.itbd.application.constants.enums.RoomTypeEnum;
+//import com.itbd.application.constants.enums.UnitTypeEnum;
+//import com.itbd.application.dao.AbstractEntity;
+//import jakarta.annotation.Nullable;
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//
 
-import com.itbd.application.constants.enums.RoomTypeEnum;
-import com.itbd.application.constants.enums.UnitTypeEnum;
-import com.itbd.application.dao.AbstractEntity;
-import jakarta.annotation.Nullable;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Set;
-
-@Entity
-@Getter
-@Setter
-@Table(name = "t_place_room", uniqueConstraints = {@UniqueConstraint(name = "ctx_place_room_unique", columnNames = {"name", "floor"})})
-public class RoomDao extends AbstractEntity<Long> {
-    @Id
-    @Column(name = "id_room_key", nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Version
-    @Nullable
-    @Column(name = "id_room_ver", nullable = false)
-    private Long version;
-
-    @Column(name = "tx_name", unique = true)
-    private String name;
-
-    @Column(name = "tx_alternate_name")
-    private String alternateName;
-
-    @Enumerated(EnumType.STRING)
-    private RoomTypeEnum type;
-
-    @Column(name = "tx_description")
-    private String description;
-
-    @Column(name = "tx_contact")
-    private String contact;
-
-    @Column(name = "has_public_access")
-    private Boolean hasPublicAccess;
-
-    @Column(name = "ct_total_rooms")
-    private Integer totalRooms;
-
-    @Column(name = "tx_block")
-    private String block;
-
-    @Column(name = "tx_unit")
-    private UnitTypeEnum unit;
-
-    @Column(name = "ct_total_beds")
-    private Integer totalBeds;
-
-    @Column(name = "ct_total_bathrooms")
-    private Integer totalBathrooms;
-
-    @Column(name = "has_kitchen")
-    private Boolean hasKitchen;
-
-    @Column(name = "has_balcony")
-    private Boolean hasBalcony;
-
-    @Column(name = "has_air_conditioner")
-    private Boolean hasAirConditioner;
-
-    @Column(name = "has_heater")
-    private Boolean hasHeater;
-
-    @ManyToOne
-    @JoinColumn(name = "id_floor_key")
-    private FloorDao floor;
-}
+//
+//import java.util.Set;
+//
+//@Entity
+//@Getter
+//@Setter
+//@Table(name = "t_place_room", uniqueConstraints = {@UniqueConstraint(name = "ctx_place_room_unique", columnNames = {"name", "floor"})})
+//public class RoomDao extends AbstractEntity<Long> {
+//    @Id
+//    @Column(name = "id_room_key", nullable = false, updatable = false)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Version
+//    @Nullable
+//    @Column(name = "id_room_ver", nullable = false)
+//    private Long version;
+//
+//    @Column(name = "tx_name", unique = true)
+//    private String name;
+//
+//    @Column(name = "tx_alternate_name")
+//    private String alternateName;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoomTypeEnum type;
+//
+//    @Column(name = "tx_description")
+//    private String description;
+//
+//    @Column(name = "tx_contact")
+//    private String contact;
+//
+//    @Column(name = "has_public_access")
+//    private Boolean hasPublicAccess;
+//
+//    @Column(name = "ct_total_rooms")
+//    private Integer totalRooms;
+//
+//    @Column(name = "tx_block")
+//    private String block;
+//
+//    @Column(name = "tx_unit")
+//    private UnitTypeEnum unit;
+//
+//    @Column(name = "ct_total_beds")
+//    private Integer totalBeds;
+//
+//    @Column(name = "ct_total_bathrooms")
+//    private Integer totalBathrooms;
+//
+//    @Column(name = "has_kitchen")
+//    private Boolean hasKitchen;
+//
+//    @Column(name = "has_balcony")
+//    private Boolean hasBalcony;
+//
+//    @Column(name = "has_air_conditioner")
+//    private Boolean hasAirConditioner;
+//
+//    @Column(name = "has_heater")
+//    private Boolean hasHeater;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "id_floor_key")
+//    private FloorDao floor;
+//}
--- src/main/java/com/itbd/application/dto/org/place/RoomDto.java
+++ src/main/java/com/itbd/application/dto/org/place/RoomDto.java
@@ -1,62 +1,62 @@
-package com.itbd.application.dto.org.place;
-
-import com.itbd.application.constants.enums.RoomTypeEnum;
-import com.itbd.application.dao.org.place.BuildingDao;
-import com.itbd.application.dao.org.place.FloorDao;
-import com.itbd.application.dao.org.place.RoomDao;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.Version;
-
-public record RoomDto(
-        Long id,
-        @Version Long version,
-        @NotNull @NotEmpty String name,
-        String alternateName,
-        RoomTypeEnum type,
-        String description,
-        String contact,
-        Boolean hasPublicAccess,
-        Integer totalRooms,
-        String block,
-        Integer totalBeds,
-        FloorDao floor) {
-    public static RoomDto fromEntity(RoomDao room) {
-        FloorDao floor = room.getFloor();
-        BuildingDao building = floor.getBuilding();
-        building.setFloors(null);
-        building.setSector(null);
-
-        floor.setBuilding(building);
-        floor.setRooms(null);
-        room.setFloor(floor);
-        return new RoomDto(
-                room.getId(),
-                room.getVersion(),
-                room.getName(),
-                room.getAlternateName(),
-                room.getType(),
-                room.getDescription(),
-                room.getContact(),
-                room.getHasPublicAccess(),
-                room.getTotalRooms(),
-                room.getBlock(),
-                room.getTotalBeds(),
-                room.getFloor());
-    }
-
-    public static void fromDTO(RoomDto roomDTO, RoomDao roomDAO) {
-        roomDAO.setId(roomDTO.id());
-        roomDAO.setVersion(roomDTO.version());
-        roomDAO.setName(roomDTO.name());
-        roomDAO.setAlternateName(roomDTO.alternateName());
-        roomDAO.setType(roomDTO.type());
-        roomDAO.setDescription(roomDTO.description());
-        roomDAO.setContact(roomDTO.contact());
-        roomDAO.setHasPublicAccess(roomDTO.hasPublicAccess());
-        roomDAO.setTotalRooms(roomDTO.totalRooms());
-        roomDAO.setBlock(roomDTO.block());
-        roomDAO.setTotalBeds(roomDTO.totalBeds());
-        roomDAO.setFloor(roomDTO.floor());
-    }
-}
+//package com.itbd.application.dto.org.place;
+//
+//import com.itbd.application.constants.enums.RoomTypeEnum;
+//import com.itbd.application.dao.org.place.BuildingDao;
+//import com.itbd.application.dao.org.place.FloorDao;
+//import com.itbd.application.dao.org.place.RoomDao;
+//import jakarta.validation.constraints.NotEmpty;
+//import jakarta.validation.constraints.NotNull;
+//import org.springframework.data.annotation.Version;
+//
+//public record RoomDto(
+//        Long id,
+//        @Version Long version,
+//        @NotNull @NotEmpty String name,
+//        String alternateName,
+//        RoomTypeEnum type,
+//        String description,
+//        String contact,
+//        Boolean hasPublicAccess,
+//        Integer totalRooms,
+//        String block,
+//        Integer totalBeds,
+//        FloorDao floor) {
+//    public static RoomDto fromEntity(RoomDao room) {
+//        FloorDao floor = room.getFloor();
+//        BuildingDao building = floor.getBuilding();
+//        building.setFloors(null);
+//        building.setSector(null);
+//
+//        floor.setBuilding(building);
+//        floor.setRooms(null);
+//        room.setFloor(floor);
+//        return new RoomDto(
+//                room.getId(),
+//                room.getVersion(),
+//                room.getName(),
+//                room.getAlternateName(),
+//                room.getType(),
+//                room.getDescription(),
+//                room.getContact(),
+//                room.getHasPublicAccess(),
+//                room.getTotalRooms(),
+//                room.getBlock(),
+//                room.getTotalBeds(),
+//                room.getFloor());
+//    }
+//
+//    public static void fromDTO(RoomDto roomDTO, RoomDao roomDAO) {
+//        roomDAO.setId(roomDTO.id());
+//        roomDAO.setVersion(roomDTO.version());
+//        roomDAO.setName(roomDTO.name());
+//        roomDAO.setAlternateName(roomDTO.alternateName());
+//        roomDAO.setType(roomDTO.type());
+//        roomDAO.setDescription(roomDTO.description());
+//        roomDAO.setContact(roomDTO.contact());
+//        roomDAO.setHasPublicAccess(roomDTO.hasPublicAccess());
+//        roomDAO.setTotalRooms(roomDTO.totalRooms());
+//        roomDAO.setBlock(roomDTO.block());
+//        roomDAO.setTotalBeds(roomDTO.totalBeds());
+//        roomDAO.setFloor(roomDTO.floor());
+//    }
+//}
