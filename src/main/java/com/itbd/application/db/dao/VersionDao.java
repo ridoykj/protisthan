f8c17ca8b75022cf4682d4f20ce66c5eb292d7b6
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:34:40 +0600

    Add VersionDao, VersionDto, and VersionRepository classes

---
+++ src/main/java/com/itbd/application/db/dao/VersionDao.java
@@ -0,0 +1,66 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_version", indexes = {
+        @Index(name = "idx_ref_doc_type_doc_name_index", columnList = "tx_ref_doc_type, tx_doc_name"),
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class VersionDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_ref_doc_type", length = 140)
+    private String refDocType;
+
+    @Column(name = "tx_doc_name", length = 140)
+    private String docName;
+
+    @Column(name = "tx_data", columnDefinition = "longtext")
+    private String data;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/VersionDto.java
@@ -0,0 +1,60 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.VersionDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record VersionDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String refDocType,
+        String docName,
+        String data,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static VersionDto fromEntity(VersionDao eVersionDao) {
+        return new VersionDto(
+                eVersionDao.getName(),
+                eVersionDao.getCreation(),
+                eVersionDao.getModified(),
+                eVersionDao.getModifiedBy(),
+                eVersionDao.getOwner(),
+                eVersionDao.getIsDocStatus(),
+                eVersionDao.getIdx(),
+                eVersionDao.getRefDocType(),
+                eVersionDao.getDocName(),
+                eVersionDao.getData(),
+                eVersionDao.getUserTags(),
+                eVersionDao.getComments(),
+                eVersionDao.getAssign(),
+                eVersionDao.getLikedBy());
+    }
+
+    public static void fromDTO(VersionDto vVersionDto, VersionDao eVersionDao) {
+        eVersionDao.setName(vVersionDto.name());
+        eVersionDao.setCreation(vVersionDto.creation());
+        eVersionDao.setModified(vVersionDto.modified());
+        eVersionDao.setModifiedBy(vVersionDto.modifiedBy());
+        eVersionDao.setOwner(vVersionDto.owner());
+        eVersionDao.setIsDocStatus(vVersionDto.isDocStatus());
+        eVersionDao.setIdx(vVersionDto.idx());
+        eVersionDao.setRefDocType(vVersionDto.refDocType());
+        eVersionDao.setDocName(vVersionDto.docName());
+        eVersionDao.setData(vVersionDto.data());
+        eVersionDao.setUserTags(vVersionDto.userTags());
+        eVersionDao.setComments(vVersionDto.comments());
+        eVersionDao.setAssign(vVersionDto.assign());
+        eVersionDao.setLikedBy(vVersionDto.likedBy());
+    }
+}
