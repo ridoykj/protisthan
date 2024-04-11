d58fbe4de0de2fd287bee0acbc83076f1dd467ca
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:37:19 +0600

    Add classes for mapping various entities

--- frontend/views/home/HomeView.tsx
+++ frontend/views/home/HomeView.tsx
@@ -1,203 +1,203 @@
-import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout';
-import CardRC from "Frontend/components/card/CardRC";
-import IDashBoardRptDto from "Frontend/generated/com/itbd/application/dto/custom/IDashBoardRptDto";
-import { OrganizationDtoCrudService } from "Frontend/generated/endpoints";
-import { useEffect, useState } from "react";
+// import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout';
+// import CardRC from "Frontend/components/card/CardRC";
+// import IDashBoardRptDto from "Frontend/generated/com/itbd/application/dto/custom/IDashBoardRptDto";
+// import { OrganizationDtoCrudService } from "Frontend/generated/endpoints";
+// import { useEffect, useState } from "react";
 
-import Chart from "react-apexcharts";
+// import Chart from "react-apexcharts";
 
-export default function HomeView() {
-  const [dashData, setDashData] = useState<IDashBoardRptDto>();
+// export default function HomeView() {
+//   const [dashData, setDashData] = useState<IDashBoardRptDto>();
 
-  const data = {
-    options: {
-      chart: {
-        id: "basic-bar"
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-      }
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }
-    ]
-  };
+//   const data = {
+//     options: {
+//       chart: {
+//         id: "basic-bar"
+//       },
+//       xaxis: {
+//         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+//       }
+//     },
+//     series: [
+//       {
+//         name: "series-1",
+//         data: [30, 40, 45, 50, 49, 60, 70, 91]
+//       }
+//     ]
+//   };
 
-  const placeItems = [
-    {
-      title: 'Place',
-      content:
-        <>
-          <HorizontalLayout><div className="w-full h-full">
-            <HorizontalLayout><Chart
-              options={{
-                chart: {
-                  id: "basic-bar"
-                },
-                xaxis: {
-                  categories: ['Sector', 'Building', 'Floor', 'Room']
-                },
-                plotOptions: {
-                  bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                  }
-                },
-                dataLabels: {
-                  enabled: true
-                },
-              }}
-              series={[
-                {
-                  name: "Place",
-                  data: [
-                    dashData?.sectors ?? 0,
-                    dashData?.buildings ?? 0,
-                    dashData?.floors ?? 0,
-                    dashData?.rooms ?? 0
-                  ]
-                }
-              ]}
-              type="bar"
-              height={200} /></HorizontalLayout>
-          </div></HorizontalLayout><HorizontalLayout><div className="w-full h-full">
-            <HorizontalLayout><Chart
-              options={{
-                chart: {
-                  id: "basic-bar"
-                },
-                xaxis: {
-                  categories: ['Sector', 'Building', 'Floor', 'Room']
-                },
-                plotOptions: {
-                  bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                  }
-                },
-                dataLabels: {
-                  enabled: true
-                },
-              }}
-              series={[
-                {
-                  name: "Place",
-                  data: [
-                    dashData?.sectors ?? 0,
-                    dashData?.buildings ?? 0,
-                    dashData?.floors ?? 0,
-                    dashData?.rooms ?? 0
-                  ]
-                }
-              ]}
-              type="bar"
-              height={200} /></HorizontalLayout>
-          </div></HorizontalLayout>
-        </>
-    },
-    {
-      title: 'Organization',
-      content:
-        <>
-          <div className="w-full h-full">
-            <Chart
-              options={{
-                chart: {
-                  id: "basic-bar"
-                },
-                xaxis: {
-                  categories: ['Profile', 'Department', 'Programme', 'Batch', 'Course']
-                },
-                plotOptions: {
-                  bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                  }
-                },
-                dataLabels: {
-                  enabled: true
-                },
-              }}
-              series={[
-                {
-                  name: "Organization",
-                  data: [
-                    dashData?.profiles ?? 0,
-                    dashData?.departments ?? 0,
-                    dashData?.programmes ?? 0,
-                    dashData?.batches ?? 0,
-                    dashData?.courses ?? 0
-                  ]
-                }
-              ]}
-              type="bar"
-              height={200} />
-          </div>
-        </>
-    },
-    {
-      title: 'Users',
-      content:
-        <>
-          <div className="w-full h-full">
-            <Chart
-              options={{
-                chart: {
-                  id: "basic-bar"
-                },
-                xaxis: {
-                  categories: ['Authnication', 'Coordinator', 'Student',]
-                },
-                plotOptions: {
-                  bar: {
-                    borderRadius: 4,
-                    horizontal: true,
-                  }
-                },
-                dataLabels: {
-                  enabled: true
-                },
-              }}
-              series={[
-                {
-                  name: "Organization",
-                  data: [
-                    0,
-                    dashData?.coordinators ?? 0,
-                    0,
-                  ]
-                }
-              ]}
-              type="bar"
-              height={200} />
-          </div>
-        </>
-    },
-  ]
+//   const placeItems = [
+//     {
+//       title: 'Place',
+//       content:
+//         <>
+//           <HorizontalLayout><div className="w-full h-full">
+//             <HorizontalLayout><Chart
+//               options={{
+//                 chart: {
+//                   id: "basic-bar"
+//                 },
+//                 xaxis: {
+//                   categories: ['Sector', 'Building', 'Floor', 'Room']
+//                 },
+//                 plotOptions: {
+//                   bar: {
+//                     borderRadius: 4,
+//                     horizontal: true,
+//                   }
+//                 },
+//                 dataLabels: {
+//                   enabled: true
+//                 },
+//               }}
+//               series={[
+//                 {
+//                   name: "Place",
+//                   data: [
+//                     dashData?.sectors ?? 0,
+//                     dashData?.buildings ?? 0,
+//                     dashData?.floors ?? 0,
+//                     dashData?.rooms ?? 0
+//                   ]
+//                 }
+//               ]}
+//               type="bar"
+//               height={200} /></HorizontalLayout>
+//           </div></HorizontalLayout><HorizontalLayout><div className="w-full h-full">
+//             <HorizontalLayout><Chart
+//               options={{
+//                 chart: {
+//                   id: "basic-bar"
+//                 },
+//                 xaxis: {
+//                   categories: ['Sector', 'Building', 'Floor', 'Room']
+//                 },
+//                 plotOptions: {
+//                   bar: {
+//                     borderRadius: 4,
+//                     horizontal: true,
+//                   }
+//                 },
+//                 dataLabels: {
+//                   enabled: true
+//                 },
+//               }}
+//               series={[
+//                 {
+//                   name: "Place",
+//                   data: [
+//                     dashData?.sectors ?? 0,
+//                     dashData?.buildings ?? 0,
+//                     dashData?.floors ?? 0,
+//                     dashData?.rooms ?? 0
+//                   ]
+//                 }
+//               ]}
+//               type="bar"
+//               height={200} /></HorizontalLayout>
+//           </div></HorizontalLayout>
+//         </>
+//     },
+//     {
+//       title: 'Organization',
+//       content:
+//         <>
+//           <div className="w-full h-full">
+//             <Chart
+//               options={{
+//                 chart: {
+//                   id: "basic-bar"
+//                 },
+//                 xaxis: {
+//                   categories: ['Profile', 'Department', 'Programme', 'Batch', 'Course']
+//                 },
+//                 plotOptions: {
+//                   bar: {
+//                     borderRadius: 4,
+//                     horizontal: true,
+//                   }
+//                 },
+//                 dataLabels: {
+//                   enabled: true
+//                 },
+//               }}
+//               series={[
+//                 {
+//                   name: "Organization",
+//                   data: [
+//                     dashData?.profiles ?? 0,
+//                     dashData?.departments ?? 0,
+//                     dashData?.programmes ?? 0,
+//                     dashData?.batches ?? 0,
+//                     dashData?.courses ?? 0
+//                   ]
+//                 }
+//               ]}
+//               type="bar"
+//               height={200} />
+//           </div>
+//         </>
+//     },
+//     {
+//       title: 'Users',
+//       content:
+//         <>
+//           <div className="w-full h-full">
+//             <Chart
+//               options={{
+//                 chart: {
+//                   id: "basic-bar"
+//                 },
+//                 xaxis: {
+//                   categories: ['Authnication', 'Coordinator', 'Student',]
+//                 },
+//                 plotOptions: {
+//                   bar: {
+//                     borderRadius: 4,
+//                     horizontal: true,
+//                   }
+//                 },
+//                 dataLabels: {
+//                   enabled: true
+//                 },
+//               }}
+//               series={[
+//                 {
+//                   name: "Organization",
+//                   data: [
+//                     0,
+//                     dashData?.coordinators ?? 0,
+//                     0,
+//                   ]
+//                 }
+//               ]}
+//               type="bar"
+//               height={200} />
+//           </div>
+//         </>
+//     },
+//   ]
 
-  useEffect(() => {
-    OrganizationDtoCrudService.getDashBoardRpt()
-      .then((res) => {
-        console.log('allData', res);
-        setDashData(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [])
+//   useEffect(() => {
+//     OrganizationDtoCrudService.getDashBoardRpt()
+//       .then((res) => {
+//         console.log('allData', res);
+//         setDashData(res);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, [])
 
-  return (
-    <>
-      <div className="bg-gray-50 h-full">
-        <div className="p-6">
-          <CardRC contents={placeItems} />
-        </div>
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div className="bg-gray-50 h-full">
+//         <div className="p-6">
+//           <CardRC contents={placeItems} />
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 
 
