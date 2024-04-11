3c004cb97a634ea1ef412f64d7f8a25ede410a04
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:33:05 +0600

    Add ErrorLogDao and ErrorLogDto classes for mapping ErrorLog entity

---
+++ src/main/java/com/itbd/application/db/dao/ErrorLogDao.java
@@ -0,0 +1,76 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_error_log", indexes = {
+        @Index(name = "idx_reference_doc_type", columnList = "tx_reference_doc_type"),
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class ErrorLogDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "is_seen", nullable = false)
+    @ColumnDefault("0")
+    private Boolean seen;
+
+    @Column(name = "tx_reference_doc_type", length = 140)
+    private String referenceDocType;
+
+    @Column(name = "tx_reference_name", length = 140)
+    private String referenceName;
+
+    @Column(name = "tx_method", length = 140)
+    private String method;
+
+    @Column(name = "tx_error", columnDefinition = "longtext")
+    private String error;
+
+    @Column(name = "tx_trace_id", length = 140)
+    private String traceId;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/ErrorLogDto.java
@@ -0,0 +1,69 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.ErrorLogDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record ErrorLogDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        Boolean seen,
+        String referenceDocType,
+        String referenceName,
+        String method,
+        String error,
+        String traceId,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static ErrorLogDto fromEntity(ErrorLogDao eErrorLogDao) {
+        return new ErrorLogDto(
+                eErrorLogDao.getName(),
+                eErrorLogDao.getCreation(),
+                eErrorLogDao.getModified(),
+                eErrorLogDao.getModifiedBy(),
+                eErrorLogDao.getOwner(),
+                eErrorLogDao.getIsDocStatus(),
+                eErrorLogDao.getIdx(),
+                eErrorLogDao.getSeen(),
+                eErrorLogDao.getReferenceDocType(),
+                eErrorLogDao.getReferenceName(),
+                eErrorLogDao.getMethod(),
+                eErrorLogDao.getError(),
+                eErrorLogDao.getTraceId(),
+                eErrorLogDao.getUserTags(),
+                eErrorLogDao.getComments(),
+                eErrorLogDao.getAssign(),
+                eErrorLogDao.getLikedBy());
+    }
+
+    public static void fromDTO(ErrorLogDto vErrorLogDto, ErrorLogDao eErrorLogDao) {
+        eErrorLogDao.setName(vErrorLogDto.name());
+        eErrorLogDao.setCreation(vErrorLogDto.creation());
+        eErrorLogDao.setModified(vErrorLogDto.modified());
+        eErrorLogDao.setModifiedBy(vErrorLogDto.modifiedBy());
+        eErrorLogDao.setOwner(vErrorLogDto.owner());
+        eErrorLogDao.setIsDocStatus(vErrorLogDto.isDocStatus());
+        eErrorLogDao.setIdx(vErrorLogDto.idx());
+        eErrorLogDao.setSeen(vErrorLogDto.seen());
+        eErrorLogDao.setReferenceDocType(vErrorLogDto.referenceDocType());
+        eErrorLogDao.setReferenceName(vErrorLogDto.referenceName());
+        eErrorLogDao.setMethod(vErrorLogDto.method());
+        eErrorLogDao.setError(vErrorLogDto.error());
+        eErrorLogDao.setTraceId(vErrorLogDto.traceId());
+        eErrorLogDao.setUserTags(vErrorLogDto.userTags());
+        eErrorLogDao.setComments(vErrorLogDto.comments());
+        eErrorLogDao.setAssign(vErrorLogDto.assign());
+        eErrorLogDao.setLikedBy(vErrorLogDto.likedBy());
+    }
+}
