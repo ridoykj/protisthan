48f31a0721f491ca96b87649102da11b0a24d9bb
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:32:22 +0600

    Add DriverDao and DriverDto classes for mapping Driver entity

---
+++ src/main/java/com/itbd/application/db/dao/DriverDao.java
@@ -0,0 +1,87 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_driver", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class DriverDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_naming_series", length = 140)
+    private String namingSeries;
+
+    @Column(name = "tx_full_name", length = 140)
+    private String fullName;
+
+    @Column(name = "tx_status", length = 140)
+    private String status;
+
+    @Column(name = "tx_transporter", length = 140)
+    private String transporter;
+
+    @Column(name = "tx_employee", length = 140)
+    private String employee;
+
+    @Column(name = "tx_cell_number", length = 140)
+    private String cellNumber;
+
+    @Column(name = "tx_address", length = 140)
+    private String address;
+
+    @Column(name = "tx_license_number", length = 140)
+    private String licenseNumber;
+
+    @Column(name = "dt_issuingDate")
+    private LocalDate issuingDate;
+
+    @Column(name = "dt_expiryDate")
+    private LocalDate expiryDate;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/DriverDto.java
@@ -0,0 +1,82 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.DriverDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record DriverDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String namingSeries,
+        String fullName,
+        String status,
+        String transporter,
+        String employee,
+        String cellNumber,
+        String address,
+        String licenseNumber,
+        LocalDate issuingDate,
+        LocalDate expiryDate,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static DriverDto fromEntity(DriverDao eDriverDao) {
+        return new DriverDto(
+                eDriverDao.getName(),
+                eDriverDao.getCreation(),
+                eDriverDao.getModified(),
+                eDriverDao.getModifiedBy(),
+                eDriverDao.getOwner(),
+                eDriverDao.getIsDocStatus(),
+                eDriverDao.getIdx(),
+                eDriverDao.getNamingSeries(),
+                eDriverDao.getFullName(),
+                eDriverDao.getStatus(),
+                eDriverDao.getTransporter(),
+                eDriverDao.getEmployee(),
+                eDriverDao.getCellNumber(),
+                eDriverDao.getAddress(),
+                eDriverDao.getLicenseNumber(),
+                eDriverDao.getIssuingDate(),
+                eDriverDao.getExpiryDate(),
+                eDriverDao.getUserTags(),
+                eDriverDao.getComments(),
+                eDriverDao.getAssign(),
+                eDriverDao.getLikedBy());
+    }
+
+    public static void fromDTO(DriverDto vDriverDto, DriverDao eDriverDao) {
+        eDriverDao.setName(vDriverDto.name());
+        eDriverDao.setCreation(vDriverDto.creation());
+        eDriverDao.setModified(vDriverDto.modified());
+        eDriverDao.setModifiedBy(vDriverDto.modifiedBy());
+        eDriverDao.setOwner(vDriverDto.owner());
+        eDriverDao.setIsDocStatus(vDriverDto.isDocStatus());
+        eDriverDao.setIdx(vDriverDto.idx());
+        eDriverDao.setNamingSeries(vDriverDto.namingSeries());
+        eDriverDao.setFullName(vDriverDto.fullName());
+        eDriverDao.setStatus(vDriverDto.status());
+        eDriverDao.setTransporter(vDriverDto.transporter());
+        eDriverDao.setEmployee(vDriverDto.employee());
+        eDriverDao.setCellNumber(vDriverDto.cellNumber());
+        eDriverDao.setAddress(vDriverDto.address());
+        eDriverDao.setLicenseNumber(vDriverDto.licenseNumber());
+        eDriverDao.setIssuingDate(vDriverDto.issuingDate());
+        eDriverDao.setExpiryDate(vDriverDto.expiryDate());
+        eDriverDao.setUserTags(vDriverDto.userTags());
+        eDriverDao.setComments(vDriverDto.comments());
+        eDriverDao.setAssign(vDriverDto.assign());
+        eDriverDao.setLikedBy(vDriverDto.likedBy());
+    }
+}
