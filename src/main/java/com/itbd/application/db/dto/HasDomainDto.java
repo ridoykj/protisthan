4b46f7d24f735765dab8fea92754b1ddb15eeabf
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:34:38 +0600

    Add HasDomainDao and HasRoleDao classes for mapping HasDomain and HasRole entities

---
+++ src/main/java/com/itbd/application/db/dto/HasDomainDto.java
@@ -0,0 +1,51 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.HasDomainDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record HasDomainDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String domain,
+        String parent,
+        String parentField,
+        String parentType
+) {
+    public static HasDomainDto fromEntity(HasDomainDao eHasDomainDao) {
+        return new HasDomainDto(
+                eHasDomainDao.getName(),
+                eHasDomainDao.getCreation(),
+                eHasDomainDao.getModified(),
+                eHasDomainDao.getModifiedBy(),
+                eHasDomainDao.getOwner(),
+                eHasDomainDao.getIsDocStatus(),
+                eHasDomainDao.getIdx(),
+                eHasDomainDao.getDomain(),
+                eHasDomainDao.getParent(),
+                eHasDomainDao.getParentField(),
+                eHasDomainDao.getParentType());
+    }
+
+    public static void fromDTO(HasDomainDto vHasDomainDto, HasDomainDao eHasDomainDao) {
+        eHasDomainDao.setName(vHasDomainDto.name());
+        eHasDomainDao.setCreation(vHasDomainDto.creation());
+        eHasDomainDao.setModified(vHasDomainDto.modified());
+        eHasDomainDao.setModifiedBy(vHasDomainDto.modifiedBy());
+        eHasDomainDao.setOwner(vHasDomainDto.owner());
+        eHasDomainDao.setIsDocStatus(vHasDomainDto.isDocStatus());
+        eHasDomainDao.setIdx(vHasDomainDto.idx());
+        eHasDomainDao.setDomain(vHasDomainDto.domain());
+        eHasDomainDao.setParent(vHasDomainDto.parent());
+        eHasDomainDao.setParentField(vHasDomainDto.parentField());
+        eHasDomainDao.setParentType(vHasDomainDto.parentType());
+    }
+}
---
+++ src/main/java/com/itbd/application/db/dto/HasRoleDto.java
@@ -0,0 +1,51 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.HasRoleDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record HasRoleDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String role,
+        String parent,
+        String parentField,
+        String parentType
+) {
+    public static HasRoleDto fromEntity(HasRoleDao eHasRoleDao) {
+        return new HasRoleDto(
+                eHasRoleDao.getName(),
+                eHasRoleDao.getCreation(),
+                eHasRoleDao.getModified(),
+                eHasRoleDao.getModifiedBy(),
+                eHasRoleDao.getOwner(),
+                eHasRoleDao.getIsDocStatus(),
+                eHasRoleDao.getIdx(),
+                eHasRoleDao.getRole(),
+                eHasRoleDao.getParent(),
+                eHasRoleDao.getParentField(),
+                eHasRoleDao.getParentType());
+    }
+
+    public static void fromDTO(HasRoleDto vHasRoleDto, HasRoleDao eHasRoleDao) {
+        eHasRoleDao.setName(vHasRoleDto.name());
+        eHasRoleDao.setCreation(vHasRoleDto.creation());
+        eHasRoleDao.setModified(vHasRoleDto.modified());
+        eHasRoleDao.setModifiedBy(vHasRoleDto.modifiedBy());
+        eHasRoleDao.setOwner(vHasRoleDto.owner());
+        eHasRoleDao.setIsDocStatus(vHasRoleDto.isDocStatus());
+        eHasRoleDao.setIdx(vHasRoleDto.idx());
+        eHasRoleDao.setRole(vHasRoleDto.role());
+        eHasRoleDao.setParent(vHasRoleDto.parent());
+        eHasRoleDao.setParentField(vHasRoleDto.parentField());
+        eHasRoleDao.setParentType(vHasRoleDto.parentType());
+    }
+}
