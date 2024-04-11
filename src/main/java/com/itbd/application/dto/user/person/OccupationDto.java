31a24d35884722fd57ee2c305daa4a5d38365057
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 17:46:52 +0600

    Refactor OccupationDao, OccupationDto, and OccupationRepo classes for better code organization and readability

--- src/main/java/com/itbd/application/dto/user/person/OccupationDto.java
+++ src/main/java/com/itbd/application/dto/user/person/OccupationDto.java
@@ -1,34 +1,34 @@
-package com.itbd.application.dto.user.person;
-
-import com.itbd.application.dao.user.person.OccupationDao;
-import org.springframework.data.annotation.Version;
-
-import java.time.LocalDateTime;
-
-public record OccupationDto(
-        Long id,
-        @Version Long version,
-        Long idPersonKey,
-        String records,
-        String hasOccupation,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-) {
-    public static OccupationDto fromEntity(OccupationDao occupation) {
-        return new OccupationDto(
-                occupation.getId(),
-                occupation.getVersion(),
-                occupation.getPerson().getId(),
-                occupation.getRecords(),
-                occupation.getHasOccupation(),
-                occupation.getCreatedAt(),
-                occupation.getUpdatedAt());
-    }
-
-    public static void fromDTO(OccupationDto occupationDTO, OccupationDao occupationDAO) {
-        occupationDAO.setId(occupationDTO.id());
-        occupationDAO.setVersion(occupationDTO.version());
-        occupationDAO.setRecords(occupationDTO.records());
-        occupationDAO.setHasOccupation(occupationDTO.hasOccupation());
-    }
-}
+//package com.itbd.application.dto.user.person;
+//
+//import com.itbd.application.dao.user.person.OccupationDao;
+//import org.springframework.data.annotation.Version;
+//
+//import java.time.LocalDateTime;
+//
+//public record OccupationDto(
+//        Long id,
+//        @Version Long version,
+//        Long idPersonKey,
+//        String records,
+//        String hasOccupation,
+//        LocalDateTime createdAt,
+//        LocalDateTime updatedAt
+//) {
+//    public static OccupationDto fromEntity(OccupationDao occupation) {
+//        return new OccupationDto(
+//                occupation.getId(),
+//                occupation.getVersion(),
+//                occupation.getPerson().getId(),
+//                occupation.getRecords(),
+//                occupation.getHasOccupation(),
+//                occupation.getCreatedAt(),
+//                occupation.getUpdatedAt());
+//    }
+//
+//    public static void fromDTO(OccupationDto occupationDTO, OccupationDao occupationDAO) {
+//        occupationDAO.setId(occupationDTO.id());
+//        occupationDAO.setVersion(occupationDTO.version());
+//        occupationDAO.setRecords(occupationDTO.records());
+//        occupationDAO.setHasOccupation(occupationDTO.hasOccupation());
+//    }
+//}
