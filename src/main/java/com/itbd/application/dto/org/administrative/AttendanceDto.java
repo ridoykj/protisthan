586bd62dbd73837de78734d6b39a814849f25b88
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:24:42 +0600

    Refactor AttendanceRepo and AttendanceDao classes

--- src/main/java/com/itbd/application/dto/org/administrative/AttendanceDto.java
+++ src/main/java/com/itbd/application/dto/org/administrative/AttendanceDto.java
@@ -1,40 +1,40 @@
-package com.itbd.application.dto.org.administrative;
-
-import com.itbd.application.dao.org.administrative.AttendanceDao;
-import org.springframework.data.annotation.Version;
-
-import java.time.LocalDateTime;
-
-public record AttendanceDto(
-        Long id,
-        @Version Long version,
-        Long registrationId,
-        LocalDateTime inTime,
-        LocalDateTime outTime,
-        Double stayingHour,
-        String status,
-        Boolean present) {
-
-    public static AttendanceDto fromEntity(AttendanceDao attendance) {
-        return new AttendanceDto(
-                attendance.getId(),
-                attendance.getVersion(),
-                attendance.getRegistrationId(),
-                attendance.getInTime(),
-                attendance.getOutTime(),
-                attendance.getStayingHour(),
-                attendance.getStatus(),
-                attendance.getPresent());
-    }
-
-    public static void fromDTO(AttendanceDto attendanceDTO, AttendanceDao attendanceDAO) {
-        attendanceDAO.setId(attendanceDTO.id());
-        attendanceDAO.setVersion(attendanceDTO.version());
-        attendanceDAO.setRegistrationId(attendanceDTO.registrationId());
-        attendanceDAO.setInTime(attendanceDTO.inTime());
-        attendanceDAO.setOutTime(attendanceDTO.outTime());
-        attendanceDAO.setStayingHour(attendanceDTO.stayingHour());
-        attendanceDAO.setStatus(attendanceDTO.status());
-        attendanceDAO.setPresent(attendanceDTO.present());
-    }
-}
+//package com.itbd.application.dto.org.administrative;
+//
+//import com.itbd.application.dao.org.administrative.AttendanceDao;
+//import org.springframework.data.annotation.Version;
+//
+//import java.time.LocalDateTime;
+//
+//public record AttendanceDto(
+//        Long id,
+//        @Version Long version,
+//        Long registrationId,
+//        LocalDateTime inTime,
+//        LocalDateTime outTime,
+//        Double stayingHour,
+//        String status,
+//        Boolean present) {
+//
+//    public static AttendanceDto fromEntity(AttendanceDao attendance) {
+//        return new AttendanceDto(
+//                attendance.getId(),
+//                attendance.getVersion(),
+//                attendance.getRegistrationId(),
+//                attendance.getInTime(),
+//                attendance.getOutTime(),
+//                attendance.getStayingHour(),
+//                attendance.getStatus(),
+//                attendance.getPresent());
+//    }
+//
+//    public static void fromDTO(AttendanceDto attendanceDTO, AttendanceDao attendanceDAO) {
+//        attendanceDAO.setId(attendanceDTO.id());
+//        attendanceDAO.setVersion(attendanceDTO.version());
+//        attendanceDAO.setRegistrationId(attendanceDTO.registrationId());
+//        attendanceDAO.setInTime(attendanceDTO.inTime());
+//        attendanceDAO.setOutTime(attendanceDTO.outTime());
+//        attendanceDAO.setStayingHour(attendanceDTO.stayingHour());
+//        attendanceDAO.setStatus(attendanceDTO.status());
+//        attendanceDAO.setPresent(attendanceDTO.present());
+//    }
+//}
