42a22536883577de36f82dd3c3f4b75f686d3eeb
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:44:48 +0600

    Add NavbarItemDao, NavbarItemDto, and NavbarItemRepository classes for mapping Navbar Item-related entities

---
+++ src/main/java/com/itbd/application/db/dao/NavbarItemDao.java
@@ -0,0 +1,73 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_navbar_item", indexes = {
+        @Index(name = "idx_parent", columnList = "tx_parent")
+})
+public class NavbarItemDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_item_label", length = 140)
+    private String itemLabel;
+
+    @Column(name = "tx_item_type", length = 140)
+    private String itemType;
+
+    @Column(name = "tx_route", length = 140)
+    private String route;
+
+    @Column(name = "tx_action", length = 140)
+    private String action;
+
+    @Column(name = "is_hidden", nullable = false)
+    @ColumnDefault("0")
+    private Boolean hidden;
+
+    @Column(name = "is_standard", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isStandard;
+
+    @Column(name = "tx_parent", length = 140)
+    private String parent;
+
+    @Column(name = "tx_parent_field", length = 140)
+    private String parentField;
+
+    @Column(name = "tx_parent_type", length = 140)
+    private String parentType;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/NavbarItemDto.java
@@ -0,0 +1,66 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.NavbarItemDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record NavbarItemDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String itemLabel,
+        String itemType,
+        String route,
+        String action,
+        Boolean hidden,
+        Boolean isStandard,
+        String parent,
+        String parentField,
+        String parentType
+) {
+    public static NavbarItemDto fromEntity(NavbarItemDao eNavbarItemDao) {
+        return new NavbarItemDto(
+                eNavbarItemDao.getName(),
+                eNavbarItemDao.getCreation(),
+                eNavbarItemDao.getModified(),
+                eNavbarItemDao.getModifiedBy(),
+                eNavbarItemDao.getOwner(),
+                eNavbarItemDao.getIsDocStatus(),
+                eNavbarItemDao.getIdx(),
+                eNavbarItemDao.getItemLabel(),
+                eNavbarItemDao.getItemType(),
+                eNavbarItemDao.getRoute(),
+                eNavbarItemDao.getAction(),
+                eNavbarItemDao.getHidden(),
+                eNavbarItemDao.getIsStandard(),
+                eNavbarItemDao.getParent(),
+                eNavbarItemDao.getParentField(),
+                eNavbarItemDao.getParentType());
+    }
+
+    public static void fromDTO(NavbarItemDto vNavbarItemDto, NavbarItemDao eNavbarItemDao) {
+        eNavbarItemDao.setName(vNavbarItemDto.name());
+        eNavbarItemDao.setCreation(vNavbarItemDto.creation());
+        eNavbarItemDao.setModified(vNavbarItemDto.modified());
+        eNavbarItemDao.setModifiedBy(vNavbarItemDto.modifiedBy());
+        eNavbarItemDao.setOwner(vNavbarItemDto.owner());
+        eNavbarItemDao.setIsDocStatus(vNavbarItemDto.isDocStatus());
+        eNavbarItemDao.setIdx(vNavbarItemDto.idx());
+        eNavbarItemDao.setItemLabel(vNavbarItemDto.itemLabel());
+        eNavbarItemDao.setItemType(vNavbarItemDto.itemType());
+        eNavbarItemDao.setRoute(vNavbarItemDto.route());
+        eNavbarItemDao.setAction(vNavbarItemDto.action());
+        eNavbarItemDao.setHidden(vNavbarItemDto.hidden());
+        eNavbarItemDao.setIsStandard(vNavbarItemDto.isStandard());
+        eNavbarItemDao.setParent(vNavbarItemDto.parent());
+        eNavbarItemDao.setParentField(vNavbarItemDto.parentField());
+        eNavbarItemDao.setParentType(vNavbarItemDto.parentType());
+    }
+}
