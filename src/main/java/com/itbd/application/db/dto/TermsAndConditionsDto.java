cde235ee071cfee824ee1e1b8ec257c4d72f3999
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:28:41 +0600

    Add TermsAndConditionsDao, TermsAndConditionsDto, and TermsAndConditionsRepository classes

---
+++ src/main/java/com/itbd/application/db/dto/TermsAndConditionsDto.java
@@ -0,0 +1,66 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.TermsAndConditionsDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record TermsAndConditionsDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String title,
+        Boolean disabled,
+        Boolean selling,
+        Boolean buying,
+        String terms,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static TermsAndConditionsDto fromEntity(TermsAndConditionsDao eTermsAndConditionsDao) {
+        return new TermsAndConditionsDto(
+                eTermsAndConditionsDao.getName(),
+                eTermsAndConditionsDao.getCreation(),
+                eTermsAndConditionsDao.getModified(),
+                eTermsAndConditionsDao.getModifiedBy(),
+                eTermsAndConditionsDao.getOwner(),
+                eTermsAndConditionsDao.getIsDocStatus(),
+                eTermsAndConditionsDao.getIdx(),
+                eTermsAndConditionsDao.getTitle(),
+                eTermsAndConditionsDao.getDisabled(),
+                eTermsAndConditionsDao.getSelling(),
+                eTermsAndConditionsDao.getBuying(),
+                eTermsAndConditionsDao.getTerms(),
+                eTermsAndConditionsDao.getUserTags(),
+                eTermsAndConditionsDao.getComments(),
+                eTermsAndConditionsDao.getAssign(),
+                eTermsAndConditionsDao.getLikedBy());
+    }
+
+    public static void fromDTO(TermsAndConditionsDto vTermsAndConditionsDto, TermsAndConditionsDao eTermsAndConditionsDao) {
+        eTermsAndConditionsDao.setName(vTermsAndConditionsDto.name());
+        eTermsAndConditionsDao.setCreation(vTermsAndConditionsDto.creation());
+        eTermsAndConditionsDao.setModified(vTermsAndConditionsDto.modified());
+        eTermsAndConditionsDao.setModifiedBy(vTermsAndConditionsDto.modifiedBy());
+        eTermsAndConditionsDao.setOwner(vTermsAndConditionsDto.owner());
+        eTermsAndConditionsDao.setIsDocStatus(vTermsAndConditionsDto.isDocStatus());
+        eTermsAndConditionsDao.setIdx(vTermsAndConditionsDto.idx());
+        eTermsAndConditionsDao.setTitle(vTermsAndConditionsDto.title());
+        eTermsAndConditionsDao.setDisabled(vTermsAndConditionsDto.disabled());
+        eTermsAndConditionsDao.setSelling(vTermsAndConditionsDto.selling());
+        eTermsAndConditionsDao.setBuying(vTermsAndConditionsDto.buying());
+        eTermsAndConditionsDao.setTerms(vTermsAndConditionsDto.terms());
+        eTermsAndConditionsDao.setUserTags(vTermsAndConditionsDto.userTags());
+        eTermsAndConditionsDao.setComments(vTermsAndConditionsDto.comments());
+        eTermsAndConditionsDao.setAssign(vTermsAndConditionsDto.assign());
+        eTermsAndConditionsDao.setLikedBy(vTermsAndConditionsDto.likedBy());
+    }
+}
---
+++ src/main/java/com/itbd/application/db/repos/TermsAndConditionsRepository.java
@@ -0,0 +1,10 @@
+package com.itbd.application.db.repos;
+
+
+import com.itbd.application.db.dao.TermsAndConditionsDao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+
+public interface TermsAndConditionsRepository extends JpaRepository<TermsAndConditionsDao, String>, JpaSpecificationExecutor<TermsAndConditionsDao> {
+}
