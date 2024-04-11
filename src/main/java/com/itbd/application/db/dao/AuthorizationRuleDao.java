ccc5bfee65e6140a048d279985c974f1cc900976
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:16:02 +0600

    Add AuthorizationRuleDao and AuthorizationRuleDto classes

---
+++ src/main/java/com/itbd/application/db/dao/AuthorizationRuleDao.java
@@ -0,0 +1,94 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_authorization_rule", indexes = {
+        @Index(name = "idx_modified", columnList = "dtt_modified")
+})
+public class AuthorizationRuleDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_transaction", length = 140)
+    private String transaction;
+
+    @Column(name = "tx_based_on", length = 140)
+    private String basedOn;
+
+    @Column(name = "tx_customer_or_item", length = 140)
+    private String customerOrItem;
+
+    @Column(name = "tx_master_name", length = 140)
+    private String masterName;
+
+    @Column(name = "tx_company", length = 140)
+    private String company;
+
+    @Column(name = "flt_value", nullable = false, precision = 21, scale = 9)
+    @ColumnDefault("0.0")
+    private BigDecimal value;
+
+    @Column(name = "tx_system_role", length = 140)
+    private String systemRole;
+
+    @Column(name = "tx_to_emp", length = 140)
+    private String toEmp;
+
+    @Column(name = "tx_system_user", length = 140)
+    private String systemUser;
+
+    @Column(name = "tx_to_designation", length = 140)
+    private String toDesignation;
+
+    @Column(name = "tx_approving_role", length = 140)
+    private String approvingRole;
+
+    @Column(name = "tx_approving_user", length = 140)
+    private String approvingUser;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/AuthorizationRuleDto.java
@@ -0,0 +1,88 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.AuthorizationRuleDao;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record AuthorizationRuleDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String transaction,
+        String basedOn,
+        String customerOrItem,
+        String masterName,
+        String company,
+        BigDecimal value,
+        String systemRole,
+        String toEmp,
+        String systemUser,
+        String toDesignation,
+        String approvingRole,
+        String approvingUser,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static AuthorizationRuleDto fromEntity(AuthorizationRuleDao eAuthorizationRuleDao) {
+        return new AuthorizationRuleDto(
+                eAuthorizationRuleDao.getName(),
+                eAuthorizationRuleDao.getCreation(),
+                eAuthorizationRuleDao.getModified(),
+                eAuthorizationRuleDao.getModifiedBy(),
+                eAuthorizationRuleDao.getOwner(),
+                eAuthorizationRuleDao.getIsDocStatus(),
+                eAuthorizationRuleDao.getIdx(),
+                eAuthorizationRuleDao.getTransaction(),
+                eAuthorizationRuleDao.getBasedOn(),
+                eAuthorizationRuleDao.getCustomerOrItem(),
+                eAuthorizationRuleDao.getMasterName(),
+                eAuthorizationRuleDao.getCompany(),
+                eAuthorizationRuleDao.getValue(),
+                eAuthorizationRuleDao.getSystemRole(),
+                eAuthorizationRuleDao.getToEmp(),
+                eAuthorizationRuleDao.getSystemUser(),
+                eAuthorizationRuleDao.getToDesignation(),
+                eAuthorizationRuleDao.getApprovingRole(),
+                eAuthorizationRuleDao.getApprovingUser(),
+                eAuthorizationRuleDao.getUserTags(),
+                eAuthorizationRuleDao.getComments(),
+                eAuthorizationRuleDao.getAssign(),
+                eAuthorizationRuleDao.getLikedBy());
+    }
+
+    public static void fromDTO(AuthorizationRuleDto vAuthorizationRuleDto, AuthorizationRuleDao eAuthorizationRuleDao) {
+        eAuthorizationRuleDao.setName(vAuthorizationRuleDto.name());
+        eAuthorizationRuleDao.setCreation(vAuthorizationRuleDto.creation());
+        eAuthorizationRuleDao.setModified(vAuthorizationRuleDto.modified());
+        eAuthorizationRuleDao.setModifiedBy(vAuthorizationRuleDto.modifiedBy());
+        eAuthorizationRuleDao.setOwner(vAuthorizationRuleDto.owner());
+        eAuthorizationRuleDao.setIsDocStatus(vAuthorizationRuleDto.isDocStatus());
+        eAuthorizationRuleDao.setIdx(vAuthorizationRuleDto.idx());
+        eAuthorizationRuleDao.setTransaction(vAuthorizationRuleDto.transaction());
+        eAuthorizationRuleDao.setBasedOn(vAuthorizationRuleDto.basedOn());
+        eAuthorizationRuleDao.setCustomerOrItem(vAuthorizationRuleDto.customerOrItem());
+        eAuthorizationRuleDao.setMasterName(vAuthorizationRuleDto.masterName());
+        eAuthorizationRuleDao.setCompany(vAuthorizationRuleDto.company());
+        eAuthorizationRuleDao.setValue(vAuthorizationRuleDto.value());
+        eAuthorizationRuleDao.setSystemRole(vAuthorizationRuleDto.systemRole());
+        eAuthorizationRuleDao.setToEmp(vAuthorizationRuleDto.toEmp());
+        eAuthorizationRuleDao.setSystemUser(vAuthorizationRuleDto.systemUser());
+        eAuthorizationRuleDao.setToDesignation(vAuthorizationRuleDto.toDesignation());
+        eAuthorizationRuleDao.setApprovingRole(vAuthorizationRuleDto.approvingRole());
+        eAuthorizationRuleDao.setApprovingUser(vAuthorizationRuleDto.approvingUser());
+        eAuthorizationRuleDao.setUserTags(vAuthorizationRuleDto.userTags());
+        eAuthorizationRuleDao.setComments(vAuthorizationRuleDto.comments());
+        eAuthorizationRuleDao.setAssign(vAuthorizationRuleDto.assign());
+        eAuthorizationRuleDao.setLikedBy(vAuthorizationRuleDto.likedBy());
+    }
+}
