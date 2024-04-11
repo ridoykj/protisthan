a4cba9a4a42855d1553bf96a70b93174c8df2eb4
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 16:25:15 +0600

    Add AllowedToTransactWithDto class for mapping AllowedToTransactWithDao entity

---
+++ src/main/java/com/itbd/application/db/dto/AllowedToTransactWithDto.java
@@ -0,0 +1,51 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.AllowedToTransactWithDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record AllowedToTransactWithDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String company,
+        String parent,
+        String parentField,
+        String parentType
+) {
+    public static AllowedToTransactWithDto fromEntity(AllowedToTransactWithDao eAllowedToTransactWithDao) {
+        return new AllowedToTransactWithDto(
+                eAllowedToTransactWithDao.getName(),
+                eAllowedToTransactWithDao.getCreation(),
+                eAllowedToTransactWithDao.getModified(),
+                eAllowedToTransactWithDao.getModifiedBy(),
+                eAllowedToTransactWithDao.getOwner(),
+                eAllowedToTransactWithDao.getIsDocStatus(),
+                eAllowedToTransactWithDao.getIdx(),
+                eAllowedToTransactWithDao.getCompany(),
+                eAllowedToTransactWithDao.getParent(),
+                eAllowedToTransactWithDao.getParentField(),
+                eAllowedToTransactWithDao.getParentType());
+    }
+
+    public static void fromDTO(AllowedToTransactWithDto vAllowedToTransactWithDto, AllowedToTransactWithDao eAllowedToTransactWithDao) {
+        eAllowedToTransactWithDao.setName(vAllowedToTransactWithDto.name());
+        eAllowedToTransactWithDao.setCreation(vAllowedToTransactWithDto.creation());
+        eAllowedToTransactWithDao.setModified(vAllowedToTransactWithDto.modified());
+        eAllowedToTransactWithDao.setModifiedBy(vAllowedToTransactWithDto.modifiedBy());
+        eAllowedToTransactWithDao.setOwner(vAllowedToTransactWithDto.owner());
+        eAllowedToTransactWithDao.setIsDocStatus(vAllowedToTransactWithDto.isDocStatus());
+        eAllowedToTransactWithDao.setIdx(vAllowedToTransactWithDto.idx());
+        eAllowedToTransactWithDao.setCompany(vAllowedToTransactWithDto.company());
+        eAllowedToTransactWithDao.setParent(vAllowedToTransactWithDto.parent());
+        eAllowedToTransactWithDao.setParentField(vAllowedToTransactWithDto.parentField());
+        eAllowedToTransactWithDao.setParentType(vAllowedToTransactWithDto.parentType());
+    }
+}
