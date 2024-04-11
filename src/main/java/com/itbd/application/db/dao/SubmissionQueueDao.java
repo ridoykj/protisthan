ab9de350f0ad28a232c13d6977acc602fe1fb631
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-11 18:17:22 +0600

    Add SubmissionQueueDao, SubmissionQueueDto, and SubmissionQueueRepository classes

---
+++ src/main/java/com/itbd/application/db/dao/SubmissionQueueDao.java
@@ -0,0 +1,75 @@
+package com.itbd.application.db.dao;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_submission_queue", indexes = {
+        @Index(name = "idx_ref_doc_name", columnList = "tx_ref_doc_name"),
+        @Index(name = "idx_modified", columnList = "dtt_modified"),
+})
+public class SubmissionQueueDao {
+
+    @Id
+    @Column(name = "tx_name", nullable = false, updatable = false, length = 140)
+    private String name;
+
+    @Column(name = "dtt_creation")
+    private LocalDateTime creation;
+
+    @Column(name = "dtt_modified")
+    private LocalDateTime modified;
+
+    @Column(name = "tx_modified_by", length = 140)
+    private String modifiedBy;
+
+    @Column(name = "tx_owner", length = 140)
+    private String owner;
+
+    @Column(name = "is_doc_status", nullable = false)
+    @ColumnDefault("0")
+    private Boolean isDocStatus;
+
+    @Column(name = "ct_idx", nullable = false)
+    @ColumnDefault("0")
+    private Integer idx;
+
+    @Column(name = "tx_status", length = 140)
+    private String status;
+
+    @Column(name = "tx_job_id", length = 140)
+    private String jobId;
+
+    @Column(name = "dtt_ended_at")
+    private LocalDateTime endedAt;
+
+    @Column(name = "tx_ref_doc_type", length = 140)
+    private String refDocType;
+
+    @Column(name = "tx_ref_doc_name", length = 140)
+    private String refDocName;
+
+    @Column(name = "tx_exception", columnDefinition = "longtext")
+    private String exception;
+
+    @Column(name = "_user_tags", columnDefinition = "text")
+    private String userTags;
+
+    @Column(name = "_comments", columnDefinition = "text")
+    private String comments;
+
+    @Column(name = "_assign", columnDefinition = "text")
+    private String assign;
+
+    @Column(name = "_liked_by", columnDefinition = "text")
+    private String likedBy;
+
+}
---
+++ src/main/java/com/itbd/application/db/dto/SubmissionQueueDto.java
@@ -0,0 +1,69 @@
+package com.itbd.application.db.dto;
+
+import com.itbd.application.db.dao.SubmissionQueueDao;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public record SubmissionQueueDto(
+        @Id
+
+        String name,
+        LocalDateTime creation,
+        LocalDateTime modified,
+        String modifiedBy,
+        String owner,
+        Boolean isDocStatus,
+        Integer idx,
+        String status,
+        String jobId,
+        LocalDateTime endedAt,
+        String refDocType,
+        String refDocName,
+        String exception,
+        String userTags,
+        String comments,
+        String assign,
+        String likedBy
+) {
+    public static SubmissionQueueDto fromEntity(SubmissionQueueDao eSubmissionQueueDao) {
+        return new SubmissionQueueDto(
+                eSubmissionQueueDao.getName(),
+                eSubmissionQueueDao.getCreation(),
+                eSubmissionQueueDao.getModified(),
+                eSubmissionQueueDao.getModifiedBy(),
+                eSubmissionQueueDao.getOwner(),
+                eSubmissionQueueDao.getIsDocStatus(),
+                eSubmissionQueueDao.getIdx(),
+                eSubmissionQueueDao.getStatus(),
+                eSubmissionQueueDao.getJobId(),
+                eSubmissionQueueDao.getEndedAt(),
+                eSubmissionQueueDao.getRefDocType(),
+                eSubmissionQueueDao.getRefDocName(),
+                eSubmissionQueueDao.getException(),
+                eSubmissionQueueDao.getUserTags(),
+                eSubmissionQueueDao.getComments(),
+                eSubmissionQueueDao.getAssign(),
+                eSubmissionQueueDao.getLikedBy());
+    }
+
+    public static void fromDTO(SubmissionQueueDto vSubmissionQueueDto, SubmissionQueueDao eSubmissionQueueDao) {
+        eSubmissionQueueDao.setName(vSubmissionQueueDto.name());
+        eSubmissionQueueDao.setCreation(vSubmissionQueueDto.creation());
+        eSubmissionQueueDao.setModified(vSubmissionQueueDto.modified());
+        eSubmissionQueueDao.setModifiedBy(vSubmissionQueueDto.modifiedBy());
+        eSubmissionQueueDao.setOwner(vSubmissionQueueDto.owner());
+        eSubmissionQueueDao.setIsDocStatus(vSubmissionQueueDto.isDocStatus());
+        eSubmissionQueueDao.setIdx(vSubmissionQueueDto.idx());
+        eSubmissionQueueDao.setStatus(vSubmissionQueueDto.status());
+        eSubmissionQueueDao.setJobId(vSubmissionQueueDto.jobId());
+        eSubmissionQueueDao.setEndedAt(vSubmissionQueueDto.endedAt());
+        eSubmissionQueueDao.setRefDocType(vSubmissionQueueDto.refDocType());
+        eSubmissionQueueDao.setRefDocName(vSubmissionQueueDto.refDocName());
+        eSubmissionQueueDao.setException(vSubmissionQueueDto.exception());
+        eSubmissionQueueDao.setUserTags(vSubmissionQueueDto.userTags());
+        eSubmissionQueueDao.setComments(vSubmissionQueueDto.comments());
+        eSubmissionQueueDao.setAssign(vSubmissionQueueDto.assign());
+        eSubmissionQueueDao.setLikedBy(vSubmissionQueueDto.likedBy());
+    }
+}
