344b9176a4ae6b8133af1734e7797c178e6c57f9
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-15 07:22:43 +0600

    Refactor SpeedDialRC component to add tooltip prop and update styling

--- frontend/components/fab/SpeedDialRC.tsx
+++ frontend/components/fab/SpeedDialRC.tsx
@@ -7,35 +7,41 @@ type ChildrenProps = {
     icon: ReactElement<any, any>;
     onClick: () => void;
 }
-const SpeedDialRC = ({ children }: { children: ChildrenProps[] }) => {
+const SpeedDialRC = ({ children, tooltip }: { children: ChildrenProps[], tooltip?: boolean }) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleOpen = () => setIsOpen(!isOpen);
 
     return (
-        <div className="absolute bottom-5 end-5 group">
+        <div className="absolute bottom-5 end-5 group items-end">
             {isOpen && (
-                <div className="flex flex-col items-center mb-4 space-y-2">
+                <div className="flex flex-col items-end mb-4 space-y-2">
                     {
                         children.map((child, index) => (
-                            <div key={`speed_dial_child_${index}`}>
+                            <div key={`speed_dial_child_${index}`} className='inline-flex items-center text-gray-500 hover:text-gray-900 hover:font-bold space-x-2'>
+                                {
+                                    tooltip == false || tooltip == undefined &&
+                                    <span className="bg-white hover:bg-gray-50 font-medium rounded-xl py-1 px-4 border border-gray-200 focus:ring-4 focus:ring-gray-300 focus:outline-none shadow-lg shadow-gray-900/20">{child.name}</span>
+                                }
                                 <button type="button"
-                                    className={'relative w-[52px] h-[52px] text-gray-500 bg-white rounded-full border border-gray-200 hover:text-gray-900 dark:border-gray-600 dark:hover:text-white dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400 shadow-lg shadow-gray-900/20 hover:shadow-gray-900/40'}
+                                    className={'relative w-[52px] h-[52px] rounded-full border border-gray-200 hover:bg-gray-50  focus:ring-4 focus:ring-gray-300 focus:outline-none shadow-lg shadow-gray-900/20 hover:shadow-gray-900/40'}
                                     data-tooltip-id={`speed_dial_child_${index}`} onClick={child.onClick}>
                                     {child.icon && React.cloneElement(child.icon, { className: "w-5 h-5 mx-auto" })}
                                     <span className="sr-only">{child.name}</span>
                                 </button>
-                                <ReactTooltip id={`speed_dial_child_${index}`} place="left" content={child.name} />
+                                {tooltip == true && <ReactTooltip id={`speed_dial_child_${index}`} place="left" content={child.name} />}
                             </div>)
                         )
                     }
                 </div>)
             }
-            <button type="button"
-                className={'flex items-center justify-center text-white bg-pink-600 rounded-full w-14 h-14 hover:bg-pink-500 dark:bg-pink-500 dark:hover:bg-pink-700 focus:ring-4 focus:ring-pink-300 focus:outline-none dark:focus:ring-pink-800 shadow-lg shadow-gray-900/20 hover:shadow-gray-900/40'}
-                onClick={toggleOpen}>
-                <FaPlus className="w-5 h-5 transition-transform group-hover:rotate-45" />
-                <span className="sr-only">Open actions menu</span>
-            </button>
+            <div className='flex flex-col items-end'>
+                <button type="button"
+                    className={'flex items-center justify-center text-white bg-pink-600 rounded-full w-14 h-14 hover:bg-pink-500  focus:ring-4 focus:ring-pink-300 focus:outline-none shadow-gray-900/20 hover:shadow-gray-900/40'}
+                    onClick={toggleOpen}>
+                    <FaPlus className="w-5 h-5 transition-transform group-hover:rotate-45" />
+                    <span className="sr-only">Open actions menu</span>
+                </button>
+            </div>
         </div>
     );
 };
