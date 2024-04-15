45f4008db409af5b201b2e42e325aca808a4065d
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-15 07:28:44 +0600

    Fix image alt attribute in GridView.tsx

--- frontend/views/grid/GridView.tsx
+++ frontend/views/grid/GridView.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from "react";
-
 export default function GridView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <img style={{ width: '200px' }} src="images/empty-plant.png" alt="not-found" />
       <h2>This place intentionally left empty</h2>
       <p>It’s a place where you can grow your own UI 🤗</p>
     </div>
