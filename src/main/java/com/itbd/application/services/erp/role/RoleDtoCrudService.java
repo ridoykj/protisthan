6837fdd3f01c75dbc0b561f352c39502ca266d79
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-15 21:33:06 +0600

    Refactor RoleDtoCrudService package to match file location

--- src/main/java/com/itbd/application/services/erp/role/RoleDtoCrudService.java
+++ src/main/java/com/itbd/application/services/erp/role/RoleDtoCrudService.java
@@ -1,4 +1,4 @@
-package com.itbd.application.services.erp;
+package com.itbd.application.services.erp.role;
 
 import com.itbd.application.db.dao.RoleDao;
 import com.itbd.application.db.dto.RoleDto;
