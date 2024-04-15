759b18b34a2bcce7b940285ca330a5cbe96761cd
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-15 07:28:36 +0600

    Update DefaultView.tsx to fix image alt attribute and remove unused imports

--- frontend/views/default/DefaultView.tsx
+++ frontend/views/default/DefaultView.tsx
@@ -1,8 +1,4 @@
-import { Button } from '@hilla/react-components/Button.js';
-import { Notification } from '@hilla/react-components/Notification.js';
-import { HelloWorldService } from 'Frontend/generated/endpoints';
 import { CSSProperties, useState } from 'react';
-import { FaCheckCircle } from 'react-icons/fa';
 
 const includedFeatures = [
   'Fully equipped learning resources centre',
@@ -10,7 +6,7 @@ const includedFeatures = [
   'Teaching and learning space, equipped with interactive screen technology',
   'Separate male and female health & fitness suites',
   'Prayer rooms, a refectory, student service hub and a range of internal and external student break out zones',
-]
+];
 
 const messages: CSSProperties = {
   backgroundColor: 'lightblue',
@@ -32,7 +28,7 @@ export default function DefaultView() {
 
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <img style={{ width: '200px' }} src="images/empty-plant.png" alt="not-found" />
       <h2>This place intentionally left empty</h2>
       <p>It’s a place where you can grow your own UI 🤗</p>
     </div>
