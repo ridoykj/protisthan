1281e5670d391688edc416f4e5fec6163007faa8
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-15 07:28:24 +0600

    Refactor AppHeader component to use function component syntax, update props usage, and fix indentation

--- frontend/views/AppLayout/AppHeader.tsx
+++ frontend/views/AppLayout/AppHeader.tsx
@@ -5,27 +5,29 @@ import { FaBars } from 'react-icons/fa';
 import { FaXmark } from 'react-icons/fa6';
 
 interface AppHeaderProps {
-    showSideNav: () => void;
+  showSideNav: () => void;
 }
-const AppHeader = ({ showSideNav }: AppHeaderProps) => {
-    const currentTitle = useRouteMetadata()?.title ?? 'My App';
-    const [iconState, setIconState] = useState(false);
-    return (
-        <>
-            <div className='flex flex-row'>
-                <button type='button' className="text-white px-4" onClick={() => {
-                    setIconState(e => !e);
-                    showSideNav();
-                }}>
-                    {iconState ? <FaXmark /> : <FaBars />}
-                </button>
-                <div className={`flex flex-row`}>
-                    <h1 className="text-white text-xl">{currentTitle}</h1>
-                    <AvatarControlRC />
-                </div>
-            </div>
-        </>
-    )
+function AppHeader({ showSideNav }: AppHeaderProps) {
+  const currentTitle = useRouteMetadata()?.title ?? 'My App';
+  const [iconState, setIconState] = useState(false);
+  return (
+    <div className="flex flex-row">
+      <button
+        type="button"
+        className="text-white px-4"
+        onClick={() => {
+          setIconState((e) => !e);
+          showSideNav();
+        }}
+      >
+        {iconState ? <FaXmark /> : <FaBars />}
+      </button>
+      <div className="flex flex-row">
+        <h1 className="text-white text-xl">{currentTitle}</h1>
+        <AvatarControlRC />
+      </div>
+    </div>
+  );
 }
 
-export default AppHeader
+export default AppHeader;
