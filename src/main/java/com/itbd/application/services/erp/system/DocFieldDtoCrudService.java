e0d314275c8260bb01529fa8503ab3e80b9136cf
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-17 12:42:51 +0600

    Add DocFieldDtoCrudService for CRUD operations on DocFieldDto

---
+++ src/main/java/com/itbd/application/services/erp/system/DocFieldDtoCrudService.java
@@ -0,0 +1,60 @@
+package com.itbd.application.services.erp.system;
+
+import com.itbd.application.db.dao.doctypes.DocFieldDao;
+import com.itbd.application.db.dto.doctypes.DocFieldDto;
+import com.itbd.application.db.repos.DocFieldRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.BrowserCallable;
+import dev.hilla.Nonnull;
+import dev.hilla.Nullable;
+import dev.hilla.crud.CrudService;
+import dev.hilla.crud.JpaFilterConverter;
+import dev.hilla.crud.filter.Filter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@BrowserCallable
+@AnonymousAllowed
+public class DocFieldDtoCrudService implements CrudService<DocFieldDto, String> {
+    private final JpaFilterConverter jpaFilterConverter;
+    private final DocFieldRepository docFieldRepo;
+
+    public DocFieldDtoCrudService(DocFieldRepository docFieldRepo, JpaFilterConverter jpaFilterConverter) {
+        this.docFieldRepo = docFieldRepo;
+        this.jpaFilterConverter = jpaFilterConverter;
+    }
+
+    @Override
+    @Nonnull
+    public List<@Nonnull DocFieldDto> list(Pageable pageable, @Nullable Filter filter) {
+        // Basic list implementation that only covers pagination,
+        // but not sorting or filtering
+        Specification<DocFieldDao> spec = filter != null
+                ? jpaFilterConverter.toSpec(filter, DocFieldDao.class)
+                : Specification.anyOf();
+        Page<DocFieldDao> persons = docFieldRepo.findAll(spec, pageable);
+        return persons.stream().map(DocFieldDto::fromEntity).toList();
+    }
+
+    @Override
+    @Transactional
+    public @Nullable DocFieldDto save(DocFieldDto value) {
+        boolean check = value.name() != null && !value.name().isEmpty();
+        DocFieldDao person = check
+                ? docFieldRepo.getReferenceById(value.name())
+                : new DocFieldDao();
+
+        // person.setRecordComment(check ? "UPDATE" : "NEW");
+        DocFieldDto.fromDTO(value, person);
+        return DocFieldDto.fromEntity(docFieldRepo.save(person));
+    }
+
+    @Override
+    public void delete(String id) {
+        docFieldRepo.deleteById(id);
+    }
+}
