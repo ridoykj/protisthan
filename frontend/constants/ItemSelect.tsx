f8259d09a8a89956fe5650afec0415d19c0a817d
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-24 11:39:17 +0600

    Refactor ItemSelect constant in ItemSelect.tsx to use consistent formatting

--- frontend/constants/ItemSelect.tsx
+++ frontend/constants/ItemSelect.tsx
@@ -1,14 +1,14 @@
 export type ItemSelect = {
-    id: number,
-    item: string,
-}
+  id: number;
+  item: string;
+};
 
 export const ClassActivity: ItemSelect[] = [
-    { id: 1, item: 'Lecture' },
-    { id: 2, item: 'Lab' },
-    { id: 3, item: 'Tutorial' },
-    { id: 4, item: 'Seminar' },
-    { id: 5, item: 'Workshop' },
-    { id: 6, item: 'Exam' },
-    { id: 7, item: 'Other' }
-]
+  { id: 1, item: 'Lecture' },
+  { id: 2, item: 'Lab' },
+  { id: 3, item: 'Tutorial' },
+  { id: 4, item: 'Seminar' },
+  { id: 5, item: 'Workshop' },
+  { id: 6, item: 'Exam' },
+  { id: 7, item: 'Other' },
+];
