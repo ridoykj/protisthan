38227db81bdc25f8da8f9f46204edbc1230373ad
ridoy.kumar <ridoy.kumar@gmail.com>
2024-04-24 11:40:06 +0600

    Refactor MainLayout component to update sidebar styling and add background color

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -1,4 +1,3 @@
-
 import Placeholder from 'Frontend/components/placeholder/Placeholder';
 import { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
@@ -9,19 +8,23 @@ export default function MainLayout() {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
 
   const showSideNav = () => {
-    setSidebarVisible(e => !e);
-  }
+    setSidebarVisible((e) => !e);
+  };
 
   return (
-    <div className="flex flex-row h-screen">
-      <aside className={`bg-gray-100 overflow-hidden duration-300 ${isSidebarVisible ? 'w-64' : 'w-0'}`}>
+    // <div className="flex flex-row h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="flex flex-row h-screen bg-slate-50 text-gray-800">
+      <aside
+        className={`bg-gray-100 overflow-hidden duration-300 ${isSidebarVisible ? 'w-64' : 'w-0'}`}
+      >
         <AppNavItem />
       </aside>
       <div className="flex flex-col overflow-hidden w-screen">
         <nav className="bg-indigo-700 p-3">
           <AppHeader showSideNav={showSideNav} />
         </nav>
-        <main className='flex-1 overflow-auto bg-gray-50'>
+        {/* <main className="flex-1 overflow-auto bg-gray-50"> */}
+        <main className="flex-1 overflow-auto ">
           <Suspense fallback={<Placeholder />}>
             <Outlet />
           </Suspense>
