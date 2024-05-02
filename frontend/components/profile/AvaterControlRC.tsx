12d3d981b40bc7e70284e6000802104627cd77e7
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-02 17:37:46 +0600

    Refactor AvatarControlRC component to fix import statements and improve styling

--- frontend/components/profile/AvaterControlRC.tsx
+++ frontend/components/profile/AvaterControlRC.tsx
@@ -5,7 +5,7 @@ import { FaGear } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 
 const userNavigation = [
-  { name: 'Your Profile', icon: <FaRegUserCircle />, href: '#' },
+  { name: 'Your Profile', icon: <FaRegUserCircle />, href: 'm/user-profile' },
   { name: 'Settings', icon: <FaGear />, href: '#' },
   { name: 'Sign out', icon: <AiOutlineLogout />, href: '#' },
 ];
@@ -41,37 +41,34 @@ export default function AvatarControlRC() {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   return (
-    <>
-      {/* <div className="absolute inset-y right-5 flex flex-row items-center md:ml-6"> */}
-      <div className="absolute inset-y right-5 flex flex-row items-center md:ml-6 space-x-4">
+    <div className="absolute inset-y right-5 flex flex-row items-center md:ml-6 space-x-4">
+      <button
+        type="button"
+        className="relative flex items-center justify-center rounded-full size-8 bg-gray-200 p-1 text-gray-400 hover:text-indigo-700 focus:outline-none focus:ring focus:ring-offset-1 focus:ring-offset-indigo-50"
+      >
+        <span className="sr-only">View notifications</span>
+        <FaRegBell className="size-5" aria-hidden="true" />
+      </button>
+
+      <div className="relative">
         <button
           type="button"
-          className="relative flex items-center justify-center rounded-full size-8 bg-gray-200 p-1 text-gray-400 hover:text-indigo-700 focus:outline-none focus:ring focus:ring-offset-1 focus:ring-offset-indigo-50"
+          className="relative flex max-w-xs items-center border border-white rounded-full bg-gray-200 text-sm focus:outline-none focus:ring focus:ring-offset-1 focus:ring-offset-indigo-50"
+          onClick={() => setIsProfileOpen(!isProfileOpen)}
         >
-          <span className="sr-only">View notifications</span>
-          <FaRegBell className="size-5" aria-hidden="true" />
+          <span className="sr-only">Menu</span>
+          <img className="size-8 rounded-full" src="images/profile/profile.jpg" alt="" />
         </button>
-
-        <div className="relative">
-          <button
-            type="button"
-            className="relative flex max-w-xs items-center rounded-full bg-gray-200 text-sm focus:outline-none focus:ring focus:ring-offset-1 focus:ring-offset-indigo-50"
-            onClick={() => setIsProfileOpen(!isProfileOpen)}
-          >
-            <span className="sr-only">Menu</span>
-            <img className="size-8 rounded-full" src="icons/icon.png" alt="" />
-          </button>
-          <div
-            className={`absolute end-0 z-10 mt-2 w-56 rounded-md bg-white/90 shadow-lg  duration-300 ${isProfileOpen ? 'visible' : 'hidden'}`}
-          >
-            <div className="p-2">
-              {userNavigation.map((item, index) => (
-                <NavLinkItem key={index} name={item.name} icon={item.icon} route={item.href} />
-              ))}
-            </div>
+        <div
+          className={`absolute end-0 z-10 mt-2 w-56 rounded-md bg-white/90 shadow-lg  duration-300 ${isProfileOpen ? 'visible' : 'hidden'}`}
+        >
+          <div className="p-2">
+            {userNavigation.map((item, index) => (
+              <NavLinkItem key={index} name={item.name} icon={item.icon} route={item.href} />
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
