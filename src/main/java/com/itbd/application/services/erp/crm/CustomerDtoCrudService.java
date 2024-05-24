54c60bea7be1bccb2b13a679288716db5ad60295
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 19:32:21 +0600

    Refactor CustomerDtoCrudService package to correct location

--- src/main/java/com/itbd/application/services/erp/crm/CustomerDtoCrudService.java
+++ src/main/java/com/itbd/application/services/erp/crm/CustomerDtoCrudService.java
@@ -1,4 +1,4 @@
-package com.itbd.application.services.erp.selling.selling;
+package com.itbd.application.services.erp.crm;
 
 import com.itbd.application.db.dao.customers.CustomerDao;
 import com.itbd.application.db.dto.customers.CustomerDto;
