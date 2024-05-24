e9adc2b230210aaaa06c9f693f21b7684b012c1b
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 19:30:30 +0600

    Add EnvProp

--- src/main/java/com/itbd/application/config/EnvProp.java
+++ src/main/java/com/itbd/application/config/EnvProp.java
@@ -1,2 +1,26 @@
-package com.itbd.application.config;public class EnvProp {
+package com.itbd.application.config;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+@Endpoint
+@AnonymousAllowed
+public class EnvProp {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    public String[] getProp() {
+        return this.env.getActiveProfiles();
+    }
+
+    public String getActiveProfile() {
+        return this.activeProfile;
+    }
 }
