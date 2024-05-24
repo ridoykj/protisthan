58e81557078b3e16305f46542de3938dc99fc4b3
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 17:24:14 +0600

    chore: Reorder import statements in App.tsx for consistency

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,6 @@
-import "./main.css";
-import router from 'Frontend/routes.js';
+import { router } from 'Frontend/routes.js';
 import { RouterProvider } from 'react-router-dom';
+import './main.css';
 
 export default function App() {
   return <RouterProvider router={router} />;
