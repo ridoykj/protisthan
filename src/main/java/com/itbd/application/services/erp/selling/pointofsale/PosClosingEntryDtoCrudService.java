1a93327763164f52f61bbda10fdb0771f0194e3f
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 19:39:01 +0600

    refactor: Update PosClosingEntryDtoCrudService to use PosClosingEntryRepository instead of PosOpeningEntryRepository

--- src/main/java/com/itbd/application/services/erp/selling/pointofsale/PosClosingEntryDtoCrudService.java
+++ src/main/java/com/itbd/application/services/erp/selling/pointofsale/PosClosingEntryDtoCrudService.java
@@ -1,7 +1,8 @@
 package com.itbd.application.services.erp.selling.pointofsale;
 
-import com.itbd.application.db.dao.pos.PosOpeningEntryDao;
-import com.itbd.application.db.dto.pos.PosOpeningEntryDto;
+import com.itbd.application.db.dao.pos.PosClosingEntryDao;
+import com.itbd.application.db.dto.pos.PosClosingEntryDto;
+import com.itbd.application.db.repos.PosClosingEntryRepository;
 import com.itbd.application.db.repos.PosOpeningEntryRepository;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.BrowserCallable;
@@ -19,42 +20,42 @@ import java.util.List;
 
 @BrowserCallable
 @AnonymousAllowed
-public class PosOpeningEntryDtoCrudService implements CrudService<PosOpeningEntryDto, String> {
+public class PosClosingEntryDtoCrudService implements CrudService<PosClosingEntryDto, String> {
     private final JpaFilterConverter jpaFilterConverter;
-    private final PosOpeningEntryRepository posOpeningEntryRepo;
+    private final PosClosingEntryRepository posClosingEntryRepo;
 
-    public PosOpeningEntryDtoCrudService(PosOpeningEntryRepository posOpeningEntryRepo, JpaFilterConverter jpaFilterConverter) {
-        this.posOpeningEntryRepo = posOpeningEntryRepo;
+    public PosClosingEntryDtoCrudService(PosClosingEntryRepository posClosingEntryRepo, JpaFilterConverter jpaFilterConverter) {
+        this.posClosingEntryRepo = posClosingEntryRepo;
         this.jpaFilterConverter = jpaFilterConverter;
     }
 
     @Override
     @Nonnull
-    public List<@Nonnull PosOpeningEntryDto> list(Pageable pageable, @Nullable Filter filter) {
+    public List<@Nonnull PosClosingEntryDto> list(Pageable pageable, @Nullable Filter filter) {
         // Basic list implementation that only covers pagination,
         // but not sorting or filtering
-        Specification<PosOpeningEntryDao> spec = filter != null
-                ? jpaFilterConverter.toSpec(filter, PosOpeningEntryDao.class)
+        Specification<PosClosingEntryDao> spec = filter != null
+                ? jpaFilterConverter.toSpec(filter, PosClosingEntryDao.class)
                 : Specification.anyOf();
-        Page<PosOpeningEntryDao> persons = posOpeningEntryRepo.findAll(spec, pageable);
-        return persons.stream().map(PosOpeningEntryDto::fromEntity).toList();
+        Page<PosClosingEntryDao> persons = posClosingEntryRepo.findAll(spec, pageable);
+        return persons.stream().map(PosClosingEntryDto::fromEntity).toList();
     }
 
     @Override
     @Transactional
-    public @Nullable PosOpeningEntryDto save(PosOpeningEntryDto value) {
+    public @Nullable PosClosingEntryDto save(PosClosingEntryDto value) {
         boolean check = value.name() != null && !value.name().isEmpty();
-        PosOpeningEntryDao person = check
-                ? posOpeningEntryRepo.getReferenceById(value.name())
-                : new PosOpeningEntryDao();
+        PosClosingEntryDao person = check
+                ? posClosingEntryRepo.getReferenceById(value.name())
+                : new PosClosingEntryDao();
 
         // person.setRecordComment(check ? "UPDATE" : "NEW");
-        PosOpeningEntryDto.fromDTO(value, person);
-        return PosOpeningEntryDto.fromEntity(posOpeningEntryRepo.save(person));
+        PosClosingEntryDto.fromDTO(value, person);
+        return PosClosingEntryDto.fromEntity(posClosingEntryRepo.save(person));
     }
 
     @Override
     public void delete(String id) {
-        posOpeningEntryRepo.deleteById(id);
+        posClosingEntryRepo.deleteById(id);
     }
 }
