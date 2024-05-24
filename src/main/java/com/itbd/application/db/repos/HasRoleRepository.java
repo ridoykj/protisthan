6f1e2837b299a4ba20068dafaaf02d28a174b2e4
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 19:31:52 +0600

    feat: Delete roles by name in HasRoleRepository

--- src/main/java/com/itbd/application/db/repos/HasRoleRepository.java
+++ src/main/java/com/itbd/application/db/repos/HasRoleRepository.java
@@ -4,7 +4,16 @@ package com.itbd.application.db.repos;
 import com.itbd.application.db.dao.HasRoleDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface HasRoleRepository extends JpaRepository<HasRoleDao, String>, JpaSpecificationExecutor<HasRoleDao> {
+//    @Modifying
+//    @Query("delete from User u where u.id in ?1")
+//    void deleteWithIds(List<Integer> ids);
+
+    void deleteByNameIn(List<String> names);
 }
