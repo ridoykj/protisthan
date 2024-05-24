55d9ed2ef04cd8f6de3e2eeca5d95b9629e68b1c
ridoy.kumar <ridoy.kumar@gmail.com>
2024-05-24 17:23:55 +0600

    chore: Update npm dependency to latest stable version

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,25 +1,57 @@
 import MainLayout from 'Frontend/views/MainLayout.js';
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import ErrorView from './views/default/ErrorView';
 import AccountView from './views/erp/module/accounting/AccountView';
-import ItemsView from './views/erp/module/sale/item/ItemsView';
-import SalesInvoiceView from './views/erp/module/sale/SalesInvoiceView';
+import CompanyView from './views/erp/module/accounting/CompanyView';
+import CustomerView from './views/erp/module/accounting/CustomerView';
+import SupplierView from './views/erp/module/accounting/SupplierView';
+import CustomerGroupView from './views/erp/module/crm/CustomerGroupView';
+import LeadView from './views/erp/module/crm/LeadView';
+import TerritoryView from './views/erp/module/crm/TerritoryView';
+import CouponCodeView from './views/erp/module/selling/item/CouponCodeView';
+import ItemGroupView from './views/erp/module/selling/item/ItemGroupView';
+import ItemPriceView from './views/erp/module/selling/item/ItemPriceView';
+import ItemsView from './views/erp/module/selling/item/ItemsView';
+import PriceListView from './views/erp/module/selling/item/PriceListView';
+import PricingRuleView from './views/erp/module/selling/item/PricingRuleView';
+import ProductBundleView from './views/erp/module/selling/item/ProductBundleView';
+import PromotionalSchemeView from './views/erp/module/selling/item/PromotionalSchemeView';
+import ShippingRuleView from './views/erp/module/selling/item/ShippingRuleView';
+import LoyaltyPointEntryView from './views/erp/module/selling/pointofsale/LoyaltyPointEntryView';
+import LoyaltyProgramView from './views/erp/module/selling/pointofsale/LoyaltyProgramView';
+import PosClosingEntryView from './views/erp/module/selling/pointofsale/PosClosingEntryView';
+import PosOpeningEntryView from './views/erp/module/selling/pointofsale/PosOpeningEntryView';
+import PosProfileView from './views/erp/module/selling/pointofsale/PosProfileView';
+import BlanketOrderView from './views/erp/module/selling/selling/BlanketOrderView';
+import QuotationView from './views/erp/module/selling/selling/QuotationView';
+import SalesInvoiceView from './views/erp/module/selling/selling/SalesInvoiceView';
+import SalesOrderView from './views/erp/module/selling/selling/SalesOrderView';
+import SalesPartnerView from './views/erp/module/selling/selling/SalesPartnerView';
+import SalesPersonView from './views/erp/module/selling/selling/SalesPersonView';
+import AddressView from './views/erp/module/selling/settings/AddressView';
+import CampaignView from './views/erp/module/selling/settings/CampaignView';
+import ContactView from './views/erp/module/selling/settings/ContactView';
+import LeadSourceView from './views/erp/module/selling/settings/LeadSourceView';
+import SalesTaxesAndChargesView from './views/erp/module/selling/settings/SalesTaxesAndChargesView';
+import TermsAndConditionsView from './views/erp/module/selling/settings/TermsAndConditionsView';
 import AccessLogView from './views/erp/module/settings/log/AccessLogView';
 import ActivityLogView from './views/erp/module/settings/log/ActivityLogView';
+import BrandView from './views/erp/module/stock/BrandView';
+import StockReconciliationView from './views/erp/module/stock/StockReconciliationView';
+import UomView from './views/erp/module/stock/UomView';
+import WarehouseView from './views/erp/module/stock/WarehouseView';
 import ModuleProfileView from './views/erp/module/users/module/ModuleProfileView';
+import RolePermissionView from './views/erp/module/users/role/RolePermissionView';
 import RolesProfileView from './views/erp/module/users/role/RolesProfileView';
 import RolesView from './views/erp/module/users/role/RolesView';
-import CompanyView from './views/erp/module/users/user/CompanyView';
-import CustomerView from './views/erp/module/users/user/CustomerView';
-import SupplierView from './views/erp/module/users/user/SupplierView';
 import UserProfileView from './views/erp/module/users/user/UserProfileView';
 import UsersView from './views/erp/module/users/user/UsersView';
 import UserTypeView from './views/erp/module/users/user/UserTypeView';
 import CommonView from './views/erp/sidenav/CommonView';
 
 // const AboutView = lazy(async () => import('Frontend/views/about/AboutView'));
-
 // const prod = false;
-const prod = true;
+// const prod = true;
 
 export const routes: RouteObject[] = [
   {
@@ -27,6 +59,7 @@ export const routes: RouteObject[] = [
     element: <MainLayout />,
     handle: { title: 'Main' },
     children: [
+      { path: '*', element: <ErrorView /> },
       { path: '', element: <UserProfileView />, handle: { title: 'Home' } },
       { path: 'm/user-profile', element: <UserProfileView />, handle: { title: 'Home' } },
       { path: ':workspace', element: <CommonView />, handle: {} },
@@ -52,6 +85,12 @@ export const routes: RouteObject[] = [
         path: 'm/role-profile/:queryId',
         element: <RolesProfileView />,
         handle: { title: 'Role Profile' },
+      },
+
+      {
+        path: 'm/permission-manager',
+        element: <RolePermissionView />,
+        handle: { title: 'Role Permission' },
       },
 
       {
@@ -85,6 +124,186 @@ export const routes: RouteObject[] = [
       { path: 'm/supplier', element: <SupplierView />, handle: { title: 'Supplier' } },
       { path: 'm/supplier/:queryId', element: <SupplierView />, handle: { title: 'Supplier' } },
 
+      { path: 'm/account', element: <AccountView />, handle: { title: 'Account' } },
+      { path: 'm/account/:queryId', element: <AccountView />, handle: { title: 'Account' } },
+
+      { path: 'm/company', element: <CompanyView />, handle: { title: 'Company' } },
+      { path: 'm/company/:queryId', element: <CompanyView />, handle: { title: 'Company' } },
+
+      { path: 'm/warehouse', element: <WarehouseView />, handle: { title: 'Warehouse' } },
+      { path: 'm/warehouse/:queryId', element: <WarehouseView />, handle: { title: 'Warehouse' } },
+
+      { path: 'm/brand', element: <BrandView />, handle: { title: 'Brand' } },
+      { path: 'm/brand/:queryId', element: <BrandView />, handle: { title: 'Brand' } },
+
+      { path: 'm/uom', element: <UomView />, handle: { title: 'UOM' } },
+      { path: 'm/uom/:queryId', element: <UomView />, handle: { title: 'UOM' } },
+
+      {
+        path: 'm/stock-reconciliation',
+        element: <StockReconciliationView />,
+        handle: { title: 'Stock Reconciliation' },
+      },
+      {
+        path: 'm/stock-reconciliation/:queryId',
+        element: <StockReconciliationView />,
+        handle: { title: 'Stock Reconciliation' },
+      },
+
+      { path: 'm/lead', element: <LeadView />, handle: { title: 'Lead' } },
+      { path: 'm/lead/:queryId', element: <LeadView />, handle: { title: 'Lead' } },
+
+      {
+        path: 'm/customer-group',
+        element: <CustomerGroupView />,
+        handle: { title: 'Customer Group' },
+      },
+      {
+        path: 'm/customer-group/:queryId',
+        element: <CustomerGroupView />,
+        handle: { title: 'Customer Group' },
+      },
+
+      { path: 'm/territory', element: <TerritoryView />, handle: { title: 'Territory' } },
+      { path: 'm/territory/:queryId', element: <TerritoryView />, handle: { title: 'Territory' } },
+
+      // add items
+      { path: 'm/coupon-code', element: <CouponCodeView />, handle: { title: 'Coupon Code' } },
+      {
+        path: 'm/coupon-code/:queryId',
+        element: <CouponCodeView />,
+        handle: { title: 'Coupon Code' },
+      },
+
+      { path: 'm/item-group', element: <ItemGroupView />, handle: { title: 'Item Group' } },
+      {
+        path: 'm/item-group/:queryId',
+        element: <ItemGroupView />,
+        handle: { title: 'Item Group' },
+      },
+
+      { path: 'm/item-price', element: <ItemPriceView />, handle: { title: 'Item Price' } },
+      {
+        path: 'm/item-price/:queryId',
+        element: <ItemPriceView />,
+        handle: { title: 'Item Price' },
+      },
+
+      { path: 'm/price-list', element: <PriceListView />, handle: { title: 'Price List' } },
+      {
+        path: 'm/price-list/:queryId',
+        element: <PriceListView />,
+        handle: { title: 'Price List' },
+      },
+
+      { path: 'm/pricing-rule', element: <PricingRuleView />, handle: { title: 'Pricing Rule' } },
+      {
+        path: 'm/pricing-rule/:queryId',
+        element: <PricingRuleView />,
+        handle: { title: 'Pricing Rule' },
+      },
+
+      {
+        path: 'm/product-bundle',
+        element: <ProductBundleView />,
+        handle: { title: 'Product Bundle' },
+      },
+      {
+        path: 'm/product-bundle/:queryId',
+        element: <ProductBundleView />,
+        handle: { title: 'Product Bundle' },
+      },
+
+      {
+        path: 'm/promotional-scheme',
+        element: <PromotionalSchemeView />,
+        handle: { title: 'Promotional Scheme' },
+      },
+      {
+        path: 'm/promotional-scheme/:queryId',
+        element: <PromotionalSchemeView />,
+        handle: { title: 'Promotional Scheme' },
+      },
+
+      {
+        path: 'm/shipping-rule',
+        element: <ShippingRuleView />,
+        handle: { title: 'Shipping Rule' },
+      },
+      {
+        path: 'm/shipping-rule/:queryId',
+        element: <ShippingRuleView />,
+        handle: { title: 'Shipping Rule' },
+      },
+
+      // add items
+      {
+        path: 'm/loyalty-point-entry',
+        element: <LoyaltyPointEntryView />,
+        handle: { title: 'Loyalty Point Entry' },
+      },
+      {
+        path: 'm/loyalty-point-entry/:queryId',
+        element: <LoyaltyPointEntryView />,
+        handle: { title: 'Loyalty Point Entry' },
+      },
+
+      {
+        path: 'm/loyalty-program',
+        element: <LoyaltyProgramView />,
+        handle: { title: 'Loyalty Program' },
+      },
+      {
+        path: 'm/loyalty-program/:queryId',
+        element: <LoyaltyProgramView />,
+        handle: { title: 'Loyalty Program' },
+      },
+
+      {
+        path: 'm/pos-closing-entry',
+        element: <PosClosingEntryView />,
+        handle: { title: 'Pos Closing Entry' },
+      },
+      {
+        path: 'm/pos-closing-entry/:queryId',
+        element: <PosClosingEntryView />,
+        handle: { title: 'Pos Closing Entry' },
+      },
+
+      {
+        path: 'm/pos-opening-entry',
+        element: <PosOpeningEntryView />,
+        handle: { title: 'Pos Opening Entry' },
+      },
+      {
+        path: 'm/pos-opening-entry/:queryId',
+        element: <PosOpeningEntryView />,
+        handle: { title: 'Pos Opening Entry' },
+      },
+
+      { path: 'm/pos-profile', element: <PosProfileView />, handle: { title: 'Pos Profile' } },
+      {
+        path: 'm/pos-profile/:queryId',
+        element: <PosProfileView />,
+        handle: { title: 'Pos Profile' },
+      },
+
+      // add items
+
+      {
+        path: 'm/blanket-order',
+        element: <BlanketOrderView />,
+        handle: { title: 'Blanket Order' },
+      },
+      {
+        path: 'm/blanket-order/:queryId',
+        element: <BlanketOrderView />,
+        handle: { title: 'Blanket Order' },
+      },
+
+      { path: 'm/quotation', element: <QuotationView />, handle: { title: 'Quotation' } },
+      { path: 'm/quotation/:queryId', element: <QuotationView />, handle: { title: 'Quotation' } },
+
       {
         path: 'm/sales-invoice',
         element: <SalesInvoiceView />,
@@ -96,37 +315,79 @@ export const routes: RouteObject[] = [
         handle: { title: 'Sales Invoice' },
       },
 
-      { path: 'm/account', element: <AccountView />, handle: { title: 'Account' } },
-      { path: 'm/account/:queryId', element: <AccountView />, handle: { title: 'Account' } },
+      { path: 'm/sales-order', element: <SalesOrderView />, handle: { title: 'Sales Order' } },
+      {
+        path: 'm/sales-order/:queryId',
+        element: <SalesOrderView />,
+        handle: { title: 'Sales Order' },
+      },
 
-      { path: 'm/company', element: <CompanyView />, handle: { title: 'Company' } },
-      { path: 'm/company/:queryId', element: <CompanyView />, handle: { title: 'Company' } },
+      {
+        path: 'm/sales-partner',
+        element: <SalesPartnerView />,
+        handle: { title: 'Sales Partner' },
+      },
+      {
+        path: 'm/sales-partner/:queryId',
+        element: <SalesPartnerView />,
+        handle: { title: 'Sales Partner' },
+      },
 
-      // { path: 'm/role/:roleId', element: <RoleView />, handle: { title: 'Roles' } },
+      { path: 'm/sales-person', element: <SalesPersonView />, handle: { title: 'Sales Person' } },
+      {
+        path: 'm/sales-person/:queryId',
+        element: <SalesPersonView />,
+        handle: { title: 'Sales Person' },
+      },
 
-      // { path: 'default', element: <DefaultView />, handle: { title: 'Not Implemented' } },
-      // { path: 'shop', element: <ShopView />, handle: { title: 'Shop' } },
-      // { path: 'grid', element: <GridView />, handle: { title: 'Grid' } },
-      // { path: 'about', element: <AboutView />, handle: { title: 'About' } },
+      // Add items
 
-      // { path: 'academic/organization', element: <OrganizationView />, handle: { title: 'Profile' } },
-      // { path: 'academic/attendance', element: <AttendanceView />, handle: { title: 'Attendance' } },
+      { path: 'm/address', element: <AddressView />, handle: { title: 'Address' } },
+      { path: 'm/lead/:queryId', element: <AddressView />, handle: { title: 'Address' } },
 
-      // { path: 'place/building', element: <BuildingView />, handle: { title: 'Building' } },
-      // { path: 'place/floor', element: <FloorView />, handle: { title: 'Floor' } },
-      // { path: 'place/room', element: <RoomView />, handle: { title: 'Room' } },
-      // { path: 'place/sector', element: <SectorView />, handle: { title: 'Sector' } },
+      { path: 'm/campaign', element: <CampaignView />, handle: { title: 'Campaign' } },
+      { path: 'm/campaign/:queryId', element: <CampaignView />, handle: { title: 'Campaign' } },
 
-      // { path: 'user/users', element: <UserView />, handle: { title: 'Users' } },
+      { path: 'm/contact', element: <ContactView />, handle: { title: 'Contact' } },
+      { path: 'm/contact/:queryId', element: <ContactView />, handle: { title: 'Contact' } },
 
-      // { path: 'permission/roles', element: <RoleView />, handle: { title: 'Roles' } },
-      // { path: 'permission/resources', element: <ResourcesView />, handle: { title: 'Resources' } },
-      // { path: 'permission/authorization', element: <AuthorizationView />, handle: { title: 'Authorization' } },
+      { path: 'm/lead-source', element: <LeadSourceView />, handle: { title: 'Lead Source' } },
+      {
+        path: 'm/lead-source/:queryId',
+        element: <LeadSourceView />,
+        handle: { title: 'Lead Source' },
+      },
 
-      // { path: 'users', element: <UserView />, handle: { title: 'Users' } },
-      // { path: 'profiles', element: <ProfileView />, handle: { title: 'profiles' } },
+      {
+        path: 'm/sales-taxes-and-charges',
+        element: <SalesTaxesAndChargesView />,
+        handle: { title: 'Sales Taxes And Charges' },
+      },
+      {
+        path: 'm/sales-taxes-and-charges/:queryId',
+        element: <SalesTaxesAndChargesView />,
+        handle: { title: 'Sales Taxes And Charges' },
+      },
+
+      {
+        path: 'm/terms-and-conditions',
+        element: <TermsAndConditionsView />,
+        handle: { title: 'Terms And Conditions' },
+      },
+      {
+        path: 'm/terms-and-conditions/:queryId',
+        element: <TermsAndConditionsView />,
+        handle: { title: 'Terms And Conditions' },
+      },
+
+      // { path: 'm/lead', element: <LeadView />, handle: { title: 'Lead' } },
+      // { path: 'm/lead/:queryId', element: <LeadView />, handle: { title: 'Lead' } },
     ],
   },
 ];
 
-export default createBrowserRouter(routes, { basename: prod ? '/protisthan' : '/' });
+// export default createBrowserRouter(routes, { basename: prod ? '/protisthan' : '/' });
+
+export const router = createBrowserRouter([...routes], {
+  basename: new URL(document.baseURI).pathname,
+});
